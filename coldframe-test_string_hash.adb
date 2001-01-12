30df9354f1f4c640ef6735a6dbf0ade8371247cc
Simon Wright <simon@pushface.org>
2001-01-12 20:35:01 +0000

    Update copyright. Pick up String_Hash from Architecture.

--- coldframe-test_string_hash.adb
+++ coldframe-test_string_hash.adb
@@ -1,4 +1,4 @@
--- Copyright (c) 2001 Simon Wright <simon@pushface.org>
+-- Copyright (C) Simon Wright <simon@pushface.org>
 
 -- This package is free software; you can redistribute it and/or
 -- modify it under terms of the GNU General Public License as
@@ -12,27 +12,20 @@
 -- the Free Software Foundation, 59 Temple Place - Suite 330, Boston,
 -- MA 02111-1307, USA.
 
--- As a special exception, if other files instantiate generics from
--- this unit, or you link this unit with other files to produce an
--- executable, this unit does not by itself cause the resulting
--- executable to be covered by the GNU General Public License.  This
--- exception does not however invalidate any other reasons why the
--- executable file might be covered by the GNU Public License.
-
--- $Id: coldframe-test_string_hash.adb,v a77d4f0c0477 2001/01/07 10:09:37 simon $
+-- $Id: coldframe-test_string_hash.adb,v bf8a0244a396 2001/01/12 20:35:01 simon $
 
 with Ada.Strings.Bounded;
 with Ada.Strings.Unbounded;
 with Ada.Text_Io;
-with String_Hash;
+with Architecture.String_Hash;
 
 procedure Architecture.Test_String_Hash is
 
   package S128 is new Ada.Strings.Bounded.Generic_Bounded_Length (128);
-  function S128_Hash is new String_Hash.Bounded_Hash (S128);
+  function S128_Hash is new Architecture.String_Hash.Bounded_Hash (S128);
 
   procedure T (S : String) is
-    use String_Hash;
+    use Architecture.String_Hash;
     use S128;
     use Ada.Strings.Unbounded;
   begin
