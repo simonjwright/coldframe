fe36bf05524e203fe449633dfd906b358d4b57e4
Simon Wright <simon@pushface.org>
2001-01-14 07:05:53 +0000

    First logged version.

---
+++ t.adb
@@ -0,0 +1,193 @@
+with Ada.Calendar;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Text_Io; use Ada.Text_Io;
+with Ada.Strings.Unbounded.Text_Io; use Ada.Strings.Unbounded.Text_Io;
+with Problem_Reporting.Problem_Report;
+with Problem_Reporting.Problem_Report.Collections;
+with Problem_Reporting.Problem_Report.All_Instances;
+with Problem_Reporting.Problem_Report.Filter_Function;
+
+procedure T is
+
+  function "+" (Source : String) return Unbounded_String
+    renames To_Unbounded_String;
+
+  use type Ada.Calendar.Time;
+
+  Loops : constant := 10_000;
+
+begin
+
+  for I in 1 .. 5 loop
+    declare
+      This : Problem_Reporting.Problem_Report.Handle;
+    begin
+      This := Problem_Reporting.Problem_Report.Create ((Id => I));
+      Problem_Reporting.Problem_Report.Set_Details (This, +("detail" & I'Img));
+      Problem_Reporting.Problem_Report.Set_Reporter (This, +"simon");
+    end;
+  end loop;
+
+  for I in 0 .. 6 loop
+    declare
+      This : Problem_Reporting.Problem_Report.Handle;
+    begin
+      This := Problem_Reporting.Problem_Report.Find ((Id => I));
+      Put ("for id" & I'Img & ": ");
+      Put (" details |");
+      Put (Problem_Reporting.Problem_Report.Get_Details (This));
+      Put ("|, reporter |");
+      Put (Problem_Reporting.Problem_Report.Get_Reporter (This));
+      Put_Line ("|");
+    exception
+      when others =>
+        Put_Line ("exception for id" & I'Img);
+    end;
+  end loop;
+
+  Problem_Reporting.Problem_Report.Delete ((Id => 2));
+  Problem_Reporting.Problem_Report.Delete ((Id => 3));
+  for I in 0 .. 6 loop
+    declare
+      This : Problem_Reporting.Problem_Report.Handle;
+    begin
+      This := Problem_Reporting.Problem_Report.Find ((Id => I));
+      Put ("for id" & I'Img & ": ");
+      Put (" details |");
+      Put (Problem_Reporting.Problem_Report.Get_Details (This));
+      Put ("|, reporter |");
+      Put (Problem_Reporting.Problem_Report.Get_Reporter (This));
+      Put_Line ("|");
+    exception
+      when others =>
+        Put_Line ("exception for id" & I'Img);
+    end;
+  end loop;
+
+  declare
+    procedure Report (This : Problem_Reporting.Problem_Report.Handle;
+                      OK : out Boolean) is
+    begin
+      Put ("id:"  & Problem_Reporting.Problem_Report.Get_Id (This)'Img);
+      Put (" details: ");
+      Put (Problem_Reporting.Problem_Report.Get_Details (This));
+      Put (", reporter ");
+      Put (Problem_Reporting.Problem_Report.Get_Reporter (This));
+      New_Line;
+      OK := True;
+    end Report;
+    procedure Visit_All is
+      new Problem_Reporting.Problem_Report.Abstract_Containers.Visit (Report);
+    C : Problem_Reporting.Problem_Report.Collections.Collection;
+    It : Problem_Reporting.Problem_Report.Abstract_Containers.Iterator'Class
+       := Problem_Reporting.Problem_Report.Collections.Concrete_Collections.New_Iterator (C);
+  begin
+    C := Problem_Reporting.Problem_Report.All_Instances;
+    Visit_All (It);
+  end;
+
+  declare
+    Start : Ada.Calendar.Time := Ada.Calendar.Clock;
+    Interval : Duration;
+  begin
+    for I in 101 .. 101 + Loops - 1 loop
+      declare
+        This : Problem_Reporting.Problem_Report.Handle;
+      begin
+        This := Problem_Reporting.Problem_Report.Create ((Id => I));
+        Problem_Reporting.Problem_Report.Set_Details (This, +("detail" & I'Img));
+        Problem_Reporting.Problem_Report.Set_Reporter (This, +"simon");
+      end;
+    end loop;
+    Interval := (Ada.Calendar.Clock - Start) / Loops * 1_000_000; -- uS
+    Put_Line ("inserting ->" & Interval'Img & " uS");
+  end;
+
+  declare
+    Start : Ada.Calendar.Time := Ada.Calendar.Clock;
+    Interval : Duration;
+  begin
+    for I in 101 .. 101 + 10_000 - 1 loop
+      declare
+        This : Problem_Reporting.Problem_Report.Handle;
+      begin
+        This := Problem_Reporting.Problem_Report.Find ((Id => I));
+      end;
+    end loop;
+    Interval := (Ada.Calendar.Clock - Start) / Loops * 1_000_000; -- uS
+    Put_Line ("finding ->" & Interval'Img & " uS");
+  end;
+
+  declare
+    Interval : Duration;
+    All_Instances : Problem_Reporting.Problem_Report.Collections.Collection;
+    Start : Ada.Calendar.Time := Ada.Calendar.Clock;
+  begin
+    All_Instances := Problem_Reporting.Problem_Report.All_Instances;
+    Interval := (Ada.Calendar.Clock - Start) * 1_000_000; -- uS
+    Put_Line ("getting all instances ->" & Interval'Img & " uS");
+    Put_Line
+       ("result size"
+        & Problem_Reporting.Problem_Report.Collections.Concrete_Collections.Length (All_Instances)'Img);
+  end;
+
+  declare
+    function Reject
+       (This : Problem_Reporting.Problem_Report.Handle) return Boolean;
+    function Filter is new Problem_Reporting.Problem_Report.Filter_Function
+       (Pass => Reject);
+    function Reject
+       (This : Problem_Reporting.Problem_Report.Handle) return Boolean is
+    begin
+      return False;
+    end Reject;
+    Interval : Duration;
+    All_Instances : Problem_Reporting.Problem_Report.Collections.Collection;
+    Result : Problem_Reporting.Problem_Report.Collections.Collection;
+    Start : Ada.Calendar.Time := Ada.Calendar.Clock;
+  begin
+    All_Instances := Problem_Reporting.Problem_Report.All_Instances;
+    Result := Filter (All_Instances);
+    Interval := (Ada.Calendar.Clock - Start) / Loops * 1_000_000; -- uS
+    Put_Line
+       ("getting all instances, rejecting ->" & Interval'Img & " uS");
+    Put_Line
+       ("result size"
+        & Problem_Reporting.Problem_Report.Collections.Concrete_Collections.Length (Result)'Img);
+  end;
+
+  declare
+    function Pass
+       (This : Problem_Reporting.Problem_Report.Handle) return Boolean;
+    function Filter is new Problem_Reporting.Problem_Report.Filter_Function;
+    function Pass
+       (This : Problem_Reporting.Problem_Report.Handle) return Boolean is
+    begin
+      return True;
+    end Pass;
+    Interval : Duration;
+    All_Instances : Problem_Reporting.Problem_Report.Collections.Collection;
+    Result : Problem_Reporting.Problem_Report.Collections.Collection;
+    Start : Ada.Calendar.Time := Ada.Calendar.Clock;
+  begin
+    All_Instances := Problem_Reporting.Problem_Report.All_Instances;
+    Result := Filter (All_Instances);
+    Interval := (Ada.Calendar.Clock - Start) / Loops * 1_000_000; -- uS
+    Put_Line ("getting all instances, accepting ->" & Interval'Img & " uS");
+    Put_Line
+       ("result size"
+        & Problem_Reporting.Problem_Report.Collections.Concrete_Collections.Length (Result)'Img);
+  end;
+
+  declare
+    Start : Ada.Calendar.Time := Ada.Calendar.Clock;
+    Interval : Duration;
+  begin
+    for I in 101 .. 101 + 10_000 - 1 loop
+       Problem_Reporting.Problem_Report.Delete ((Id => I));
+    end loop;
+    Interval := (Ada.Calendar.Clock - Start) / Loops * 1_000_000; -- uS
+    Put_Line ("deleting ->" & Interval'Img & " uS");
+  end;
+
+end T;
