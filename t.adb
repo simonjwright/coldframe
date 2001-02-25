2d7b0267e3dfbc37ff4fa2bc4bca7a39a674ffe0
Simon Wright <simon@pushface.org>
2001-02-25 10:02:47 +0000

    Added Filtering checks.

--- t.adb
+++ t.adb
@@ -6,6 +6,7 @@ with Problem_Reporting.Problem_Report;
 with Problem_Reporting.Problem_Report.Collections;
 with Problem_Reporting.Problem_Report.All_Instances;
 with Problem_Reporting.Problem_Report.Filter_Function;
+with Problem_Reporting.Problem_Report.Selection_Function;
 
 procedure T is
 
@@ -18,6 +19,7 @@ procedure T is
 
 begin
 
+  -- insert some instance
   for I in 1 .. 5 loop
     declare
       This : Problem_Reporting.Problem_Report.Handle;
@@ -28,6 +30,7 @@ begin
     end;
   end loop;
 
+  -- find and report instances (including some that are missing)
   for I in 0 .. 6 loop
     declare
       This : Problem_Reporting.Problem_Report.Handle;
@@ -41,12 +44,15 @@ begin
       Put_Line ("|");
     exception
       when others =>
-        Put_Line ("exception for id" & I'Img);
+        Put_Line (" ** exception for id" & I'Img);
     end;
   end loop;
 
+  -- delete a couple of instances
   Problem_Reporting.Problem_Report.Delete ((Id => 2));
   Problem_Reporting.Problem_Report.Delete ((Id => 3));
+
+  -- find and report instances (including some more that are missing)
   for I in 0 .. 6 loop
     declare
       This : Problem_Reporting.Problem_Report.Handle;
@@ -60,10 +66,11 @@ begin
       Put_Line ("|");
     exception
       when others =>
-        Put_Line ("exception for id" & I'Img);
+        Put_Line (" ** exception for id" & I'Img);
     end;
   end loop;
 
+  -- iterate over all the instances
   declare
     procedure Report (This : Problem_Reporting.Problem_Report.Handle;
                       OK : out Boolean) is
@@ -86,6 +93,82 @@ begin
     Visit_All (It);
   end;
 
+  -- check filtering
+  for I in 7 .. 11 loop
+    declare
+      This : Problem_Reporting.Problem_Report.Handle;
+    begin
+      This := Problem_Reporting.Problem_Report.Create ((Id => I));
+      Problem_Reporting.Problem_Report.Set_Details (This, +("detail" & I'Img));
+      Problem_Reporting.Problem_Report.Set_Reporter (This, +"liz");
+    end;
+  end loop;
+  declare
+    procedure Report
+       (C : Problem_Reporting.Problem_Report.Collections.Collection) is
+      procedure Report (This : Problem_Reporting.Problem_Report.Handle;
+                        OK : out Boolean) is
+      begin
+        Put ("id:"  & Problem_Reporting.Problem_Report.Get_Id (This)'Img);
+        Put (" details: ");
+        Put (Problem_Reporting.Problem_Report.Get_Details (This));
+        Put (", reporter ");
+        Put (Problem_Reporting.Problem_Report.Get_Reporter (This));
+        New_Line;
+        OK := True;
+      end Report;
+      procedure Visit_All is
+        new Problem_Reporting.Problem_Report.Abstract_Containers.Visit (Report);
+      It : Problem_Reporting.Problem_Report.Abstract_Containers.Iterator'Class
+         := Problem_Reporting.Problem_Report.Collections.New_Iterator (C);
+    begin
+      Visit_All (It);
+    end Report;
+    function Choose
+       (This : Problem_Reporting.Problem_Report.Handle) return Boolean;
+    function Filter is new Problem_Reporting.Problem_Report.Filter_Function
+       (Pass => Choose);
+    function Choose
+       (This : Problem_Reporting.Problem_Report.Handle) return Boolean is
+    begin
+      return Problem_Reporting.Problem_Report.Get_Reporter (This) = +"liz";
+    end Choose;
+    All_Instances : Problem_Reporting.Problem_Report.Collections.Collection;
+    Result : Problem_Reporting.Problem_Report.Collections.Collection;
+  begin
+    All_Instances := Problem_Reporting.Problem_Report.All_Instances;
+    Result := Filter (All_Instances);
+    Put_Line ("all instances:");
+    Report (All_Instances);
+    Put_Line ("just liz:");
+    Report (Result);
+  end;
+
+  -- delete all instances
+  declare
+    procedure Delete is
+      procedure Delete (This : Problem_Reporting.Problem_Report.Handle;
+                        OK : out Boolean) is
+      begin
+        Problem_Reporting.Problem_Report.Delete
+           ((Id => Problem_Reporting.Problem_Report.Get_Id (This)));
+        OK := True;
+      end Delete;
+      procedure Visit_All is
+        new Problem_Reporting.Problem_Report.Abstract_Containers.Visit (Delete);
+      All_Instances : Problem_Reporting.Problem_Report.Collections.Collection
+         := Problem_Reporting.Problem_Report.All_Instances;
+      It : Problem_Reporting.Problem_Report.Abstract_Containers.Iterator'Class
+          := Problem_Reporting.Problem_Report.Collections.New_Iterator
+             (All_Instances);
+    begin
+      Visit_All (It);
+    end Delete;
+  begin
+    Delete;
+  end;
+
+  -- time inserting new instances
   declare
     Start : Ada.Calendar.Time := Ada.Calendar.Clock;
     Interval : Duration;
@@ -103,6 +186,7 @@ begin
     Put_Line ("inserting ->" & Interval'Img & " uS");
   end;
 
+  -- time finding an instance by identifier
   declare
     Start : Ada.Calendar.Time := Ada.Calendar.Clock;
     Interval : Duration;
@@ -118,6 +202,7 @@ begin
     Put_Line ("finding ->" & Interval'Img & " uS");
   end;
 
+  -- time getting all instances
   declare
     Interval : Duration;
     All_Instances : Problem_Reporting.Problem_Report.Collections.Collection;
@@ -131,10 +216,11 @@ begin
         & Problem_Reporting.Problem_Report.Collections.Length (All_Instances)'Img);
   end;
 
+  -- time selection (failure)
   declare
     function Reject
        (This : Problem_Reporting.Problem_Report.Handle) return Boolean;
-    function Filter is new Problem_Reporting.Problem_Report.Filter_Function
+    function Filter is new Problem_Reporting.Problem_Report.Selection_Function
        (Pass => Reject);
     function Reject
        (This : Problem_Reporting.Problem_Report.Handle) return Boolean is
@@ -142,43 +228,41 @@ begin
       return False;
     end Reject;
     Interval : Duration;
-    All_Instances : Problem_Reporting.Problem_Report.Collections.Collection;
     Result : Problem_Reporting.Problem_Report.Collections.Collection;
     Start : Ada.Calendar.Time := Ada.Calendar.Clock;
   begin
-    All_Instances := Problem_Reporting.Problem_Report.All_Instances;
-    Result := Filter (All_Instances);
+    Result := Filter;
     Interval := (Ada.Calendar.Clock - Start) / Loops * 1_000_000; -- uS
     Put_Line
-       ("getting all instances, rejecting ->" & Interval'Img & " uS");
+       ("rejecting ->" & Interval'Img & " uS");
     Put_Line
        ("result size"
         & Problem_Reporting.Problem_Report.Collections.Length (Result)'Img);
   end;
 
+  -- time selection (success)
   declare
     function Pass
        (This : Problem_Reporting.Problem_Report.Handle) return Boolean;
-    function Filter is new Problem_Reporting.Problem_Report.Filter_Function;
+    function Filter is new Problem_Reporting.Problem_Report.Selection_Function;
     function Pass
        (This : Problem_Reporting.Problem_Report.Handle) return Boolean is
     begin
       return True;
     end Pass;
     Interval : Duration;
-    All_Instances : Problem_Reporting.Problem_Report.Collections.Collection;
     Result : Problem_Reporting.Problem_Report.Collections.Collection;
     Start : Ada.Calendar.Time := Ada.Calendar.Clock;
   begin
-    All_Instances := Problem_Reporting.Problem_Report.All_Instances;
-    Result := Filter (All_Instances);
+    Result := Filter;
     Interval := (Ada.Calendar.Clock - Start) / Loops * 1_000_000; -- uS
-    Put_Line ("getting all instances, accepting ->" & Interval'Img & " uS");
+    Put_Line ("accepting ->" & Interval'Img & " uS");
     Put_Line
        ("result size"
         & Problem_Reporting.Problem_Report.Collections.Length (Result)'Img);
   end;
 
+  -- time deletion
   declare
     Start : Ada.Calendar.Time := Ada.Calendar.Clock;
     Interval : Duration;
