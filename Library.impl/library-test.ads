f1accb74c508a45ebb30134b7067cd23db69e14d
Simon Wright <simon@pushface.org>
2001-09-04 05:18:59 +0000

    Now supports AUnit.

--- Library.impl/library-test.ads
+++ Library.impl/library-test.ads
@@ -1,7 +1,18 @@
--- $Id: library-test.ads,v cf4daf4ea64c 2001/05/05 16:59:48 simon $
+--  Copyright (C) Simon Wright <simon@pushface.org>
 
--- The class packages being tested are private children of the domain
--- package "Library", which means that this test procedure (whose body
--- is generated using tg) has itself to be private.
+--  This package is free software; you can redistribute it and/or
+--  modify it under terms of the GNU General Public License as
+--  published by the Free Software Foundation; either version 2, or
+--  (at your option) any later version. This package is distributed in
+--  the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+--  even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+--  PARTICULAR PURPOSE. See the GNU General Public License for more
+--  details. You should have received a copy of the GNU General Public
+--  License distributed with this package; see file COPYING.  If not,
+--  write to the Free Software Foundation, 59 Temple Place - Suite
+--  330, Boston, MA 02111-1307, USA.
 
-private procedure Library.Test;
+--  $Id: library-test.ads,v 3e2a2f3218f6 2001/09/04 05:18:59 simon $
+
+with AUnit.Test_Suites; use AUnit.Test_Suites;
+function Library.Test return Access_Test_Suite;
