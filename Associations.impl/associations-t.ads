446c7f2d7887ca777272f8b5aa1dfa6923d1fb14
Simon Wright <simon@pushface.org>
2002-02-17 10:12:43 +0000

    First version.

---
+++ Associations.impl/associations-t.ads
@@ -0,0 +1,28 @@
+with Associations.Abp1;
+with Associations.Abp2;
+with Associations.Abp3;
+with Associations.Abp4;
+with Associations.Abp5;
+with Associations.Abp6;
+with Associations.Abc1;
+with Associations.Abc2;
+with Associations.Abc3;
+with Associations.Abc4;
+with Associations.Abc5;
+with Associations.Abc6;
+with Associations.Abc7;
+with Associations.Sbp1;
+with Associations.Sbp2;
+with Associations.Sbp3;
+with Associations.Sbp4;
+with Associations.Sbp5;
+with Associations.Sbp6;
+with Associations.Sbc1;
+with Associations.Sbc2;
+with Associations.Sbc3;
+with Associations.Sbc4;
+with Associations.Sbc5;
+with Associations.Sbc6;
+private package Associations.T is
+end Associations.T;
+
