2c2871f2457849891fe0d3ce8bb53521c8f0dab3
Simon Wright <simon@pushface.org>
2002-02-21 06:02:59 +0000

    Now generated.

--- States.impl/states-events.adb
+++
@@ -1,8 +0,0 @@
-with ColdFrame.States.Wall_Timer.Debug;
-package body States.Events is
---     The_Dispatcher :
---       aliased ColdFrame.States.Wall_Timer.Event_Queue;
-begin
-   --     Dispatcher := The_Dispatcher'Access;
-   Dispatcher := new ColdFrame.States.Wall_Timer.Debug.Event_Queue;
-end States.Events;
--- States.impl/states-events.ads
+++
@@ -1,7 +0,0 @@
-with ColdFrame.States;
-package States.Events is
-
-   pragma Elaborate_Body;
-   Dispatcher : ColdFrame.States.Event_Queue_P;
-
-end States.Events;
