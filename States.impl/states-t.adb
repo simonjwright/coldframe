3c6ea7f2b4ee9807e11ab688acff90781a4bdb9a
Simon Wright <simon@pushface.org>
2002-02-21 05:55:10 +0000

    ColdFrame.States -> .Events. Post accepts an event-access.

--- States.impl/states-t.adb
+++ States.impl/states-t.adb
@@ -1,24 +1,24 @@
 with GNAT.IO; use GNAT.IO;
 with ColdFrame.Exceptions.Traceback;
 pragma Warnings (Off, ColdFrame.Exceptions.Traceback);
-with ColdFrame.States;
+with ColdFrame.Events;
 with States.Initialize;
 with States.Events;
 with States.Monitor;
 procedure States.T is
-   E : ColdFrame.States.Event_P;
+   E : ColdFrame.Events.Event_P;
    M : Monitor.Handle;
 begin
    Initialize;
    M := Monitor.Find;
    E := new Monitor.Start (M);
    Put_Line ("Generating Start");
-   ColdFrame.States.Post (It => E.all, On => Events.Dispatcher);
+   ColdFrame.Events.Post (The => E, On => Events.Dispatcher);
    for N in 1 .. 10 loop
       delay 1.0;
       E := new Monitor.Heartbeat (M);
       Put_Line ("generating Heartbeat");
-      ColdFrame.States.Post (It => E.all, On => Events.Dispatcher);
+      ColdFrame.Events.Post (The => E, On => Events.Dispatcher);
    end loop;
    Put_Line ("4 second gap in Heartbeat");
    delay 4.0;
@@ -26,7 +26,7 @@ begin
       delay 1.0;
       E := new Monitor.Heartbeat (M);
       Put_Line ("generating Heartbeat");
-      ColdFrame.States.Post (It => E.all, On => Events.Dispatcher);
+      ColdFrame.Events.Post (The => E, On => Events.Dispatcher);
    end loop;
    Put_Line ("stopping Heartbeat");
 end States.T;
