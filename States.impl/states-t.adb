2c930e0df443334785c7695407e91c2a0b64d196
Simon Wright <simon@pushface.org>
2002-02-23 13:32:24 +0000

    Use the Tick message.

--- States.impl/states-t.adb
+++ States.impl/states-t.adb
@@ -7,26 +7,23 @@ with States.Events;
 with States.Monitor;
 procedure States.T is
    E : ColdFrame.Events.Event_P;
-   M : Monitor.Handle;
 begin
    Initialize;
-   M := Monitor.Find;
-   E := new Monitor.Start (M);
-   Put_Line ("Generating Start");
-   ColdFrame.Events.Post (The => E, On => Events.Dispatcher);
    for N in 1 .. 10 loop
       delay 1.0;
-      E := new Monitor.Heartbeat (M);
-      Put_Line ("generating Heartbeat");
+      E := new Monitor.Tick;
+      Monitor.Tick (E.all).The := Input;
+      Put_Line ("generating input Tick");
       ColdFrame.Events.Post (The => E, On => Events.Dispatcher);
    end loop;
-   Put_Line ("4 second gap in Heartbeat");
+   Put_Line ("4 second gap in input Tick");
    delay 4.0;
    for N in 1 .. 10 loop
       delay 1.0;
-      E := new Monitor.Heartbeat (M);
-      Put_Line ("generating Heartbeat");
+      E := new Monitor.Tick;
+      Monitor.Tick (E.all).The := Input;
+      Put_Line ("generating input Tick");
       ColdFrame.Events.Post (The => E, On => Events.Dispatcher);
    end loop;
-   Put_Line ("stopping Heartbeat");
+   Put_Line ("stopping input Tick");
 end States.T;
