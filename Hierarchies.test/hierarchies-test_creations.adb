84e266bd2a36fe872a0c7ef263bc0b7c94ba86af
Simon Wright <simon@pushface.org>
2002-05-18 07:58:18 +0000

    Continues.

--- Hierarchies.test/hierarchies-test_creations.adb
+++ Hierarchies.test/hierarchies-test_creations.adb
@@ -9,16 +9,16 @@ with Hierarchies.Tear_Down;
 with Hierarchies.R_1.Inheritance;
 with Hierarchies.R_1.All_Instances;
 with Hierarchies.R_1.Collections;
-with Hierarchies.R_2;
+with Hierarchies.R_2.Inheritance;
 with Hierarchies.R_2.All_Instances;
 with Hierarchies.R_2.Collections;
-with Hierarchies.R_3;
+with Hierarchies.R_3.Inheritance;
 with Hierarchies.R_3.All_Instances;
 with Hierarchies.R_3.Collections;
 with Hierarchies.S_2.Inheritance;
 with Hierarchies.S_2.All_Instances;
 with Hierarchies.S_2.Collections;
-with Hierarchies.S_3;
+with Hierarchies.S_3.Inheritance;
 with Hierarchies.S_3.All_Instances;
 with Hierarchies.S_3.Collections;
 with Hierarchies.T_2.Inheritance;
@@ -60,7 +60,7 @@ package body Hierarchies.Test_Creations is
       Assert (R_2.Collections.Length (R_2.All_Instances) = 0,
               "R_2 present");
       Assert (R_3.Collections.Length (R_3.All_Instances) = 0,
-              "S_3 present");
+              "R_3 present");
       Assert (S_2.Collections.Length (S_2.All_Instances) = 0,
               "S_2 present");
       Assert (S_3.Collections.Length (S_3.All_Instances) = 0,
@@ -83,7 +83,7 @@ package body Hierarchies.Test_Creations is
       Assert (R_2.Collections.Length (R_2.All_Instances) = 0,
               "R_2 present");
       Assert (R_3.Collections.Length (R_3.All_Instances) = 0,
-              "S_3 present");
+              "R_3 present");
       Assert (S_2.Collections.Length (S_2.All_Instances) = 1,
               "S_2 missing");
       Assert (S_3.Collections.Length (S_3.All_Instances) = 0,
@@ -142,32 +142,47 @@ package body Hierarchies.Test_Creations is
       Assert (R_1.Collections.Length (R_1.All_Instances) = 1,
               "R_1 missing");
       Assert (R_2.Collections.Length (R_2.All_Instances) = 1,
-              "R_2 present");
+              "R_2 missing");
       Assert (R_3.Collections.Length (R_3.All_Instances) = 1,
-              "S_3 present");
+              "R_3 missing");
       Assert (S_2.Collections.Length (S_2.All_Instances) = 1,
               "S_2 missing");
       Assert (S_3.Collections.Length (S_3.All_Instances) = 1,
-              "S_3 present");
+              "S_3 missing");
       Assert (T_2.Collections.Length (T_2.All_Instances) = 1,
               "T_2 missing");
       Assert (F_2.Collections.Length (F_2.All_Instances) = 1,
-              "F_2 present");
+              "F_2 missing");
       R1_H := R_1.Collections.First (R_1.All_Instances);
+      R2_H := R_2.Collections.First (R_2.All_Instances);
+      R3_H := R_3.Collections.First (R_3.All_Instances);
       S2_H := S_2.Collections.First (S_2.All_Instances);
+      S3_H := S_3.Collections.First (S_3.All_Instances);
       T2_H := T_2.Collections.First (T_2.All_Instances);
       Assert (CIH (R1_H) = S_2.Get_A_Parent (S2_H),
               "S_2 has wrong parent");
       Assert (S_2.Handle (R_1.Get_A_Child (R1_H).S2) = S2_H,
               "R_1 has wrong child");
+      Assert (CIH (R2_H) = S_3.Get_B_Parent (S3_H),
+              "S_3 has wrong B parent");
+      Assert (S_3.Handle (R_2.Get_B_Child (R2_H).S3) = S3_H,
+              "R_2 has wrong child");
+      Assert (CIH (R3_H) = S_3.Get_C_Parent (S3_H),
+              "S_3 has wrong C parent");
+      Assert (S_3.Handle (R_3.Get_C_Child (R3_H).S3) = S3_H,
+              "R_3 has wrong child");
       Assert (CIH (S2_H) = T_2.Get_D_Parent (T2_H),
               "T_2 has wrong parent");
       Assert (T_2.Handle (S_2.Get_D_Child (S2_H).T2) = T2_H,
               "S_2 has wrong child");
       Assert (CIH (T2_H) = F_2.Get_F_Parent (F2_H),
-              "F_2 has wrong parent");
+              "F_2 has wrong F parent");
       Assert (F_2.Handle (T_2.Get_F_Child (T2_H).F2) = F2_H,
               "T_2 has wrong child");
+      Assert (CIH (S3_H) = F_2.Get_E_Parent (F2_H),
+              "F_2 has wrong E parent");
+      Assert (F_2.Handle (S_3.Get_E_Child (S3_H).F2) = F2_H,
+              "S_3 has wrong E child");
    end Create_Third_Child;
 
    procedure Create_First_Child_With_Root
@@ -231,6 +246,188 @@ package body Hierarchies.Test_Creations is
               "S_2 has wrong child");
    end Create_Second_Child_With_First;
 
+   procedure Create_Third_Child_With_First_R1
+     (R : in out AUnit.Test_Cases.Test_Case'Class);
+   procedure Create_Third_Child_With_First_R1
+     (R : in out AUnit.Test_Cases.Test_Case'Class) is
+      pragma Warnings (Off, R);
+   begin
+      S2_H := S_2.Inheritance.Create_Tree (null);
+      F2_H := F_2.Inheritance.Create_Tree (CIH (S2_H), null, null);
+      Assert (R_1.Collections.Length (R_1.All_Instances) = 1,
+              "R_1 missing");
+      Assert (R_2.Collections.Length (R_2.All_Instances) = 1,
+              "R_2 missing");
+      Assert (R_3.Collections.Length (R_3.All_Instances) = 1,
+              "R_3 missing");
+      Assert (S_2.Collections.Length (S_2.All_Instances) = 1,
+              "S_2 missing");
+      Assert (S_3.Collections.Length (S_3.All_Instances) = 1,
+              "S_3 missing");
+      Assert (T_2.Collections.Length (T_2.All_Instances) = 1,
+              "T_2 missing");
+      Assert (F_2.Collections.Length (F_2.All_Instances) = 1,
+              "F_2 missing");
+      R1_H := R_1.Collections.First (R_1.All_Instances);
+      R2_H := R_2.Collections.First (R_2.All_Instances);
+      R3_H := R_3.Collections.First (R_3.All_Instances);
+      S3_H := S_3.Collections.First (S_3.All_Instances);
+      T2_H := T_2.Collections.First (T_2.All_Instances);
+      Assert (CIH (R1_H) = S_2.Get_A_Parent (S2_H),
+              "S_2 has wrong parent");
+      Assert (S_2.Handle (R_1.Get_A_Child (R1_H).S2) = S2_H,
+              "R_1 has wrong child");
+      Assert (CIH (R2_H) = S_3.Get_B_Parent (S3_H),
+              "S_3 has wrong B parent");
+      Assert (S_3.Handle (R_2.Get_B_Child (R2_H).S3) = S3_H,
+              "R_2 has wrong child");
+      Assert (CIH (R3_H) = S_3.Get_C_Parent (S3_H),
+              "S_3 has wrong C parent");
+      Assert (S_3.Handle (R_3.Get_C_Child (R3_H).S3) = S3_H,
+              "R_3 has wrong child");
+      Assert (CIH (S2_H) = T_2.Get_D_Parent (T2_H),
+              "T_2 has wrong parent");
+      Assert (T_2.Handle (S_2.Get_D_Child (S2_H).T2) = T2_H,
+              "S_2 has wrong child");
+      Assert (CIH (T2_H) = F_2.Get_F_Parent (F2_H),
+              "F_2 has wrong F parent");
+      Assert (F_2.Handle (T_2.Get_F_Child (T2_H).F2) = F2_H,
+              "T_2 has wrong child");
+      Assert (CIH (S3_H) = F_2.Get_E_Parent (F2_H),
+              "F_2 has wrong E parent");
+      Assert (F_2.Handle (S_3.Get_E_Child (S3_H).F2) = F2_H,
+              "S_3 has wrong E child");
+   end Create_Third_Child_With_First_R1;
+
+   procedure Create_Third_Child_With_First_R3
+     (R : in out AUnit.Test_Cases.Test_Case'Class);
+   procedure Create_Third_Child_With_First_R3
+     (R : in out AUnit.Test_Cases.Test_Case'Class) is
+      pragma Warnings (Off, R);
+   begin
+      R3_H := R_3.Inheritance.Create_Tree (null);
+      F2_H := F_2.Inheritance.Create_Tree (null, null, CIH (R3_H));
+      Assert (R_1.Collections.Length (R_1.All_Instances) = 1,
+              "R_1 missing");
+      Assert (R_2.Collections.Length (R_2.All_Instances) = 1,
+              "R_2 missing");
+      Assert (R_3.Collections.Length (R_3.All_Instances) = 1,
+              "R_3 missing");
+      Assert (S_2.Collections.Length (S_2.All_Instances) = 1,
+              "S_2 missing");
+      Assert (S_3.Collections.Length (S_3.All_Instances) = 1,
+              "S_3 missing");
+      Assert (T_2.Collections.Length (T_2.All_Instances) = 1,
+              "T_2 missing");
+      Assert (F_2.Collections.Length (F_2.All_Instances) = 1,
+              "F_2 missing");
+      R1_H := R_1.Collections.First (R_1.All_Instances);
+      R2_H := R_2.Collections.First (R_2.All_Instances);
+      S2_H := S_2.Collections.First (S_2.All_Instances);
+      S3_H := S_3.Collections.First (S_3.All_Instances);
+      T2_H := T_2.Collections.First (T_2.All_Instances);
+      Assert (CIH (R1_H) = S_2.Get_A_Parent (S2_H),
+              "S_2 has wrong parent");
+      Assert (S_2.Handle (R_1.Get_A_Child (R1_H).S2) = S2_H,
+              "R_1 has wrong child");
+      Assert (CIH (R2_H) = S_3.Get_B_Parent (S3_H),
+              "S_3 has wrong B parent");
+      Assert (S_3.Handle (R_2.Get_B_Child (R2_H).S3) = S3_H,
+              "R_2 has wrong child");
+      Assert (CIH (R3_H) = S_3.Get_C_Parent (S3_H),
+              "S_3 has wrong C parent");
+      Assert (S_3.Handle (R_3.Get_C_Child (R3_H).S3) = S3_H,
+              "R_3 has wrong child");
+      Assert (CIH (S2_H) = T_2.Get_D_Parent (T2_H),
+              "T_2 has wrong parent");
+      Assert (T_2.Handle (S_2.Get_D_Child (S2_H).T2) = T2_H,
+              "S_2 has wrong child");
+      Assert (CIH (T2_H) = F_2.Get_F_Parent (F2_H),
+              "F_2 has wrong F parent");
+      Assert (F_2.Handle (T_2.Get_F_Child (T2_H).F2) = F2_H,
+              "T_2 has wrong child");
+      Assert (CIH (S3_H) = F_2.Get_E_Parent (F2_H),
+              "F_2 has wrong E parent");
+      Assert (F_2.Handle (S_3.Get_E_Child (S3_H).F2) = F2_H,
+              "S_3 has wrong E child");
+   end Create_Third_Child_With_First_R3;
+
+   procedure Create_Third_Child_With_First_R2_R3
+     (R : in out AUnit.Test_Cases.Test_Case'Class);
+   procedure Create_Third_Child_With_First_R2_R3
+     (R : in out AUnit.Test_Cases.Test_Case'Class) is
+      pragma Warnings (Off, R);
+   begin
+      S3_H := S_3.Inheritance.Create_Tree (null, null);
+      F2_H := F_2.Inheritance.Create_Tree (null, CIH (S3_H), CIH (S3_H));
+      Assert (R_1.Collections.Length (R_1.All_Instances) = 1,
+              "R_1 missing");
+      Assert (R_2.Collections.Length (R_2.All_Instances) = 1,
+              "R_2 missing");
+      Assert (R_3.Collections.Length (R_3.All_Instances) = 1,
+              "R_3 missing");
+      Assert (S_2.Collections.Length (S_2.All_Instances) = 1,
+              "S_2 missing");
+      Assert (S_3.Collections.Length (S_3.All_Instances) = 1,
+              "S_3 missing");
+      Assert (T_2.Collections.Length (T_2.All_Instances) = 1,
+              "T_2 missing");
+      Assert (F_2.Collections.Length (F_2.All_Instances) = 1,
+              "F_2 missing");
+      R1_H := R_1.Collections.First (R_1.All_Instances);
+      R2_H := R_2.Collections.First (R_2.All_Instances);
+      R3_H := R_3.Collections.First (R_3.All_Instances);
+      S2_H := S_2.Collections.First (S_2.All_Instances);
+      S3_H := S_3.Collections.First (S_3.All_Instances);
+      T2_H := T_2.Collections.First (T_2.All_Instances);
+      Assert (CIH (R1_H) = S_2.Get_A_Parent (S2_H),
+              "S_2 has wrong parent");
+      Assert (S_2.Handle (R_1.Get_A_Child (R1_H).S2) = S2_H,
+              "R_1 has wrong child");
+      Assert (CIH (R2_H) = S_3.Get_B_Parent (S3_H),
+              "S_3 has wrong B parent");
+      Assert (S_3.Handle (R_2.Get_B_Child (R2_H).S3) = S3_H,
+              "R_2 has wrong child");
+      Assert (CIH (R3_H) = S_3.Get_C_Parent (S3_H),
+              "S_3 has wrong C parent");
+      Assert (S_3.Handle (R_3.Get_C_Child (R3_H).S3) = S3_H,
+              "R_3 has wrong child");
+      Assert (CIH (S2_H) = T_2.Get_D_Parent (T2_H),
+              "T_2 has wrong parent");
+      Assert (T_2.Handle (S_2.Get_D_Child (S2_H).T2) = T2_H,
+              "S_2 has wrong child");
+      Assert (CIH (T2_H) = F_2.Get_F_Parent (F2_H),
+              "F_2 has wrong F parent");
+      Assert (F_2.Handle (T_2.Get_F_Child (T2_H).F2) = F2_H,
+              "T_2 has wrong child");
+      Assert (CIH (S3_H) = F_2.Get_E_Parent (F2_H),
+              "F_2 has wrong E parent");
+      Assert (F_2.Handle (S_3.Get_E_Child (S3_H).F2) = F2_H,
+              "S_3 has wrong E child");
+   end Create_Third_Child_With_First_R2_R3;
+
+   procedure Create_Third_Child_With_Bad_R2_S3
+     (R : in out AUnit.Test_Cases.Test_Case'Class);
+   procedure Create_Third_Child_With_Bad_R2_S3
+     (R : in out AUnit.Test_Cases.Test_Case'Class) is
+      pragma Warnings (Off, R);
+   begin
+      S3_H := S_3.Inheritance.Create_Tree (null, null);
+      F2_H := F_2.Inheritance.Create_Tree (null, null, CIH (S3_H));
+   end Create_Third_Child_With_Bad_R2_S3;
+
+   procedure Create_Third_Child_With_Bad_S3_S3
+     (R : in out AUnit.Test_Cases.Test_Case'Class);
+   procedure Create_Third_Child_With_Bad_S3_S3
+     (R : in out AUnit.Test_Cases.Test_Case'Class) is
+      pragma Warnings (Off, R);
+      S3_H2 : S_3.Handle;
+   begin
+      S3_H := S_3.Inheritance.Create_Tree (null, null);
+      S3_H2 := S_3.Inheritance.Create_Tree (null, null);
+      F2_H := F_2.Inheritance.Create_Tree (null, CIH (S3_H), CIH (S3_H2));
+   end Create_Third_Child_With_Bad_S3_S3;
+
    procedure Register_Tests (T : in out Test_Case) is
    begin
       Register_Routine
@@ -249,6 +446,26 @@ package body Hierarchies.Test_Creations is
         (T,
          Create_Second_Child_With_First'Access,
          "Create second child with first");
+      Register_Routine
+        (T,
+         Create_Third_Child_With_First_R1'Access,
+         "Create third child with first R1");
+      Register_Routine
+        (T,
+         Create_Third_Child_With_First_R3'Access,
+         "Create third child with first R3");
+      Register_Routine
+        (T,
+         Create_Third_Child_With_First_R2_R3'Access,
+         "Create third child with first R2 & R3");
+      Register_Routine
+        (T,
+         Create_Third_Child_With_Bad_R2_S3'Access,
+         "Create third child with bad R2 & S3");
+      Register_Routine
+        (T,
+         Create_Third_Child_With_Bad_S3_S3'Access,
+         "Create third child with mismatched R2 & R3");
    end Register_Tests;
 
    function Name (T : Test_Case) return String_Access is
