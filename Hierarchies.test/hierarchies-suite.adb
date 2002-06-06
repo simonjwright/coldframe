829fadd7c816c10068c982e499e60baacd309628
Simon Wright <simon@pushface.org>
2002-06-06 07:29:22 +0000

    Added header comments.

--- Hierarchies.test/hierarchies-suite.adb
+++ Hierarchies.test/hierarchies-suite.adb
@@ -1,8 +1,28 @@
+--  Copyright (C) Simon Wright <simon@pushface.org>
+
+--  This package is free software; you can redistribute it and/or
+--  modify it under terms of the GNU General Public License as
+--  published by the Free Software Foundation; either version 2, or
+--  (at your option) any later version. This package is distributed in
+--  the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+--  even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+--  PARTICULAR PURPOSE. See the GNU General Public License for more
+--  details. You should have received a copy of the GNU General Public
+--  License distributed with this package; see file COPYING.  If not,
+--  write to the Free Software Foundation, 59 Temple Place - Suite
+--  330, Boston, MA 02111-1307, USA.
+
+--  $RCSfile: hierarchies-suite.adb,v $
+--  $Revision: efdd0e365a0e $
+--  $Date: 2002/06/06 07:29:22 $
+--  $Author: simon $
+
 with Hierarchies.Test_Creations;
 with Hierarchies.Test_Finds;
 with Hierarchies.Test_Deletions;
 
-function Hierarchies.Suite return AUnit.Test_Suites.Access_Test_Suite is
+function Hierarchies.Suite
+  return AUnit.Test_Suites.Access_Test_Suite is
    use AUnit.Test_Suites;
    Result : Access_Test_Suite := new Test_Suite;
 begin
--- Hierarchies.test/hierarchies-test_deletions.adb
+++ Hierarchies.test/hierarchies-test_deletions.adb
@@ -1,3 +1,22 @@
+--  Copyright (C) Simon Wright <simon@pushface.org>
+
+--  This package is free software; you can redistribute it and/or
+--  modify it under terms of the GNU General Public License as
+--  published by the Free Software Foundation; either version 2, or
+--  (at your option) any later version. This package is distributed in
+--  the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+--  even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+--  PARTICULAR PURPOSE. See the GNU General Public License for more
+--  details. You should have received a copy of the GNU General Public
+--  License distributed with this package; see file COPYING.  If not,
+--  write to the Free Software Foundation, 59 Temple Place - Suite
+--  330, Boston, MA 02111-1307, USA.
+
+--  $RCSfile: hierarchies-test_deletions.adb,v $
+--  $Revision: efdd0e365a0e $
+--  $Date: 2002/06/06 07:29:22 $
+--  $Author: simon $
+
 with AUnit.Test_Cases.Registration; use AUnit.Test_Cases.Registration;
 with AUnit.Assertions; use AUnit.Assertions;
 
