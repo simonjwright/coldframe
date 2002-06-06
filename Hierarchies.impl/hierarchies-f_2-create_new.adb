542d868f602c7e08df6dc1c60ec9df0c285f4f3d
Simon Wright <simon@pushface.org>
2002-06-06 07:47:21 +0000

    Added header comments.

--- Hierarchies.impl/hierarchies-f_2-create_new.adb
+++ Hierarchies.impl/hierarchies-f_2-create_new.adb
@@ -1,3 +1,22 @@
+--  Copyright (C) Simon Wright <simon@pushface.org>
+
+--  This package is free software; you can redistribute it and/or
+--  modify it under terms of the GNU General Public License as
+--  published by the Free Software Foundation; either version 2, or
+--  (at your option) any later version. This package is distributed in
+--  the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+--  even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+--  PARTICULAR PURPOSE. See the GNU General Public License for more
+--  details. You should have received a copy of the GNU General Public
+--  License distributed with this package; see file COPYING.  If not,
+--  write to the Free Software Foundation, 59 Temple Place - Suite
+--  330, Boston, MA 02111-1307, USA.
+
+--  $RCSfile: hierarchies-f_2-create_new.adb,v $
+--  $Revision: 436ec9196145 $
+--  $Date: 2002/06/06 07:47:21 $
+--  $Author: simon $
+
 with Hierarchies.F_2.Inheritance;
 
 separate (Hierarchies.F_2)
