6773b7931a36914491ec02c55f73d51ab7d28f71
Simon Wright <simon@pushface.org>
2002-07-07 18:30:10 +0000

    Removed abort of environment task, now that Tear_Down kills Domain.Events.Dispatcher.

--- Event_Test.impl/event_test-harness.adb
+++ Event_Test.impl/event_test-harness.adb
@@ -1,6 +1,5 @@
 with AUnit.Test_Runner;
 with Event_Test.Suite;
-with Ada.Task_Identification;
 
 procedure Event_Test.Harness is
 
@@ -9,7 +8,5 @@ procedure Event_Test.Harness is
 begin
 
    Run;
-
-   Ada.Task_Identification.Abort_Task (Ada.Task_Identification.Current_Task);
 
 end Event_Test.Harness;
