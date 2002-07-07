117c003cdd4c5d4db6fbeaa2dd22bab525af280a
Simon Wright <simon@pushface.org>
2002-07-07 17:50:07 +0000

    Use new generic add-in for Debug support.

--- coldframe-project-events-standard-debug.ads
+++ coldframe-project-events-standard-debug.ads
@@ -9,12 +9,12 @@
 --  This is ColdFrame's default implementation.
 
 --  $RCSfile: coldframe-project-events-standard-debug.ads,v $
---  $Revision: 6cd4886dc353 $
---  $Date: 2002/04/16 18:46:06 $
+--  $Revision: dbd92554a8bc $
+--  $Date: 2002/07/07 17:50:07 $
 --  $Author: simon $
 
-with ColdFrame.Events_G.Standard_G.Debug_G;
+with ColdFrame.Events_G.Debug_G;
 with ColdFrame.Project.Events.Standard;
 
 package ColdFrame.Project.Events.Standard.Debug
-is new Events.Standard.Debug_G;
+is new Events.Debug_G (Standard_Queue => Standard.Event_Queue);
