519aea60db28fe48e7e41fb2eb297255735c814f
Simon Wright <simon@pushface.org>
2002-07-13 18:26:21 +0000

    Removed debug output.

--- Event_Test.impl/event_test-recipient-information_handler.adb
+++ Event_Test.impl/event_test-recipient-information_handler.adb
@@ -1,13 +1,7 @@
-with GNAT.IO;
-
 separate (Event_Test.Recipient)
 procedure Information_Handler
   (I : Information) is
 begin
    This.Ordinal := I.Payload.Ordinal;
    This.Offset := Clock - I.Payload.Expected_At;
-   GNAT.IO.Put_Line ("message: ordinal" &
-                       This.Ordinal'Img &
-                       ", " &
-                       This.Offset'Img);
 end Information_Handler;
