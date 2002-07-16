cb64f9ffbfc4770d52cc8776ec383bd042007488
Simon Wright <simon@pushface.org>
2002-07-16 20:34:03 +0000

    First version.

---
+++ coldframe-project-calendar.ads
@@ -0,0 +1,19 @@
+--  This package is public-domain software; you can redistribute it
+--  and/or modify it as you wish. This package is distributed in the
+--  hope that it will be useful, but WITHOUT ANY WARRANTY; without
+--  even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+--  PARTICULAR PURPOSE.
+
+--  ColdFrame expects this package to exist to support time
+--  management.
+--
+--  This is ColdFrame's default implementation.
+
+--  $RCSfile: coldframe-project-calendar.ads,v $
+--  $Revision: 314b8fd194f4 $
+--  $Date: 2002/07/16 20:34:03 $
+--  $Author: simon $
+
+with Ada.Calendar;
+
+package ColdFrame.Project.Calendar renames Ada.Calendar;
