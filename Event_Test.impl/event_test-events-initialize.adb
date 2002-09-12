9393fb047f4ec917660abc04ebfe3ca712182ccf
Simon Wright <simon@pushface.org>
2002-09-12 20:44:31 +0000

    Use the Debugging queue.

--- Event_Test.impl/event_test-events-initialize.adb
+++ Event_Test.impl/event_test-events-initialize.adb
@@ -1,6 +1,8 @@
 with ColdFrame.Project.Events.Standard.Test;
+with Debugging;
 separate (Event_Test.Events)
 procedure Initialize is
 begin
-   Dispatcher := new ColdFrame.Project.Events.Standard.Test.Event_Queue;
+   Dispatcher := new Debugging.Debugging_Queues.Event_Queue;
+--     Dispatcher := new ColdFrame.Project.Events.Standard.Test.Event_Queue;
 end Initialize;
