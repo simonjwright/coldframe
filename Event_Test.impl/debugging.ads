3c3b05e7e2ad91126987240cd5dbef5b6cd730c7
Simon Wright <simon@pushface.org>
2002-09-13 19:25:13 +0000

    Need to instantiate with a type.

--- Event_Test.impl/debugging.ads
+++ Event_Test.impl/debugging.ads
@@ -3,6 +3,10 @@ with ColdFrame.Events_G.Debug_G;
 package Debugging is
 
    package Debugging_Queues is new ColdFrame.Project.Events.Debug_G
-     (Standard_Queue => ColdFrame.Project.Events.Standard.Test.Event_Queue);
+     (Standard_Queue =>
+        ColdFrame.Project.Events.Standard.Test.Event_Queue_Base);
+
+   subtype Event_Queue
+      is Debugging_Queues.Event_Queue (Start_Started => False);
 
 end Debugging;
