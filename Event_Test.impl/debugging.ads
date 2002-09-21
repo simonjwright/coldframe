47a0932431da51dc054604db7eea4de41ae163b9
Simon Wright <simon@pushface.org>
2002-09-21 11:08:25 +0000

    Derive a Test queue from a Debug one (so the default is not started).

--- Event_Test.impl/debugging.ads
+++ Event_Test.impl/debugging.ads
@@ -1,12 +1,12 @@
-with ColdFrame.Project.Events.Standard.Test;
-with ColdFrame.Events_G.Debug_G;
+with ColdFrame.Project.Events.Standard.Debug;
+with ColdFrame.Events_G.Test_G;
 package Debugging is
 
-   package Debugging_Queues is new ColdFrame.Project.Events.Debug_G
+   package Debugging_Queues is new ColdFrame.Project.Events.Test_G
      (Standard_Queue =>
-        ColdFrame.Project.Events.Standard.Test.Event_Queue_Base);
+        ColdFrame.Project.Events.Standard.Debug.Event_Queue_Base);
 
    subtype Event_Queue
-      is Debugging_Queues.Event_Queue (Start_Started => False);
+      is Debugging_Queues.Event_Queue;
 
 end Debugging;
