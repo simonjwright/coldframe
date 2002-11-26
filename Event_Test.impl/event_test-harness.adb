5a388ae241365b186abd52330bed21cd202aa76e
Simon Wright <simon@pushface.org>
2002-11-26 20:01:38 +0000

    Use symbolic traceback.

--- Event_Test.impl/event_test-harness.adb
+++ Event_Test.impl/event_test-harness.adb
@@ -1,7 +1,7 @@
 with AUnit.Test_Runner;
 with Event_Test.Suite;
-with ColdFrame.Exceptions.Traceback;
-pragma Warnings (Off, ColdFrame.Exceptions.Traceback);
+with ColdFrame.Exceptions.Symbolic_Traceback;
+pragma Warnings (Off, ColdFrame.Exceptions.Symbolic_Traceback);
 
 procedure Event_Test.Harness is
 
