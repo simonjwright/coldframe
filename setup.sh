dba378df0237d5227613742e8feaa28f7c1e51b6
Simon Wright <simon@pushface.org>
2003-01-04 12:22:53 +0000

    First version.

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# $Id: setup.sh,v 7cb4dbd174a9 2003/01/04 12:22:53 simon $
+# Sets up environment variables for development of the Stairwell demo (on
+# tiamat).
+ 
+export TOP=/home/simon
+export TASH=tash832a
+export TCL=/usr/lib
+export TCL_VERSION=8.3
+export BC=bc-devel
+export BUILD=/home/simon/cf/.build
+export DEVEL=YES
