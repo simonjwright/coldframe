10817e448ec05b6fce6bea3f4120705ff3e86166
Simon Wright <simon@pushface.org>
2003-01-07 20:08:34 +0000

    Added commentary, rcsid. Only call Changed if it's actually changed.

--- Digital_IO.impl/digital_io-signal-set_state.adb
+++ Digital_IO.impl/digital_io-signal-set_state.adb
@@ -1,11 +1,20 @@
+--  $Id: digital_io-signal-set_state.adb,v 3f55fc632b38 2003/01/07 20:08:34 simon $
+
+--  Set the signal's state as indicated; call Changed if it has
+--  changed.
+
 separate (Digital_IO.Signal)
 procedure Set_State
   (This : Handle;
    To : Boolean) is
 begin
 
-   This.State := To;
+   if To /= This.State then
 
-   Changed (This);
+      This.State := To;
+
+      Changed (This);
+
+   end if;
 
 end Set_State;
