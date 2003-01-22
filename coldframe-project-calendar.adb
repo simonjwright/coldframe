e3c9eaf443f589d420321663f0960873554c763d
Simon Wright <simon@pushface.org>
2003-01-22 19:53:27 +0000

    First version.

---
+++ coldframe-project-calendar.adb
@@ -0,0 +1,36 @@
+--  Copyright (C) Simon Wright <simon@pushface.org>
+
+--  This unit is free software; you can redistribute it and/or modify
+--  it as you wish. This unit is distributed in the hope that it will
+--  be useful, but WITHOUT ANY WARRANTY; without even the implied
+--  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+--  ColdFrame expects this package to exist to support time
+--  management.
+--
+--  This is ColdFrame's default implementation.
+
+--  $RCSfile: coldframe-project-calendar.adb,v $
+--  $Revision: efc523cc7629 $
+--  $Date: 2003/01/22 19:53:27 $
+--  $Author: simon $
+
+package body ColdFrame.Project.Calendar is
+
+
+   Epoch : constant Ada.Calendar.Time :=
+     Ada.Calendar.Time_Of (Year => 1970,
+                           Month => 1,
+                           Day => 1);
+
+
+   function Image (T : Time) return String is
+      use type Ada.Calendar.Time;
+      Since : constant Duration := T - Epoch;
+      --  which works just fine on GNAT!
+   begin
+      return Since'Img;
+   end Image;
+
+
+end ColdFrame.Project.Calendar;
