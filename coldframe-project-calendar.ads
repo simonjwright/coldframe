3f615030a40fea9a1ba3f9739ee4d2d87de6e89c
Simon Wright <simon@pushface.org>
2003-01-22 19:51:15 +0000

    Added Image.

--- coldframe-project-calendar.ads
+++ coldframe-project-calendar.ads
@@ -11,8 +11,8 @@
 --  This is ColdFrame's default implementation.
 
 --  $RCSfile: coldframe-project-calendar.ads,v $
---  $Revision: b720f64f3037 $
---  $Date: 2002/09/28 17:13:37 $
+--  $Revision: c02acd3df96c $
+--  $Date: 2003/01/22 19:51:15 $
 --  $Author: simon $
 
 with Ada.Calendar;
@@ -22,5 +22,10 @@ package ColdFrame.Project.Calendar is
    subtype Time is Ada.Calendar.Time;
 
    function Clock return Time renames Ada.Calendar.Clock;
+
+   function Image (T : Time) return String;
+   --  Returns a string version of Time for use in generated
+   --  Serialization.Image functions.
+   --  This implementation returns time as a Duration since 1 Jan 1970 00:00.
 
 end ColdFrame.Project.Calendar;
