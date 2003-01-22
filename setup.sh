4e9174d948d802401b201dffb169c22210e70c56
Simon Wright <simon@pushface.org>
2003-01-22 19:55:07 +0000

    Added BUILD_DIR.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-# $Id: setup.sh,v 7cb4dbd174a9 2003/01/04 12:22:53 simon $
+# $Id: setup.sh,v 86e556e75755 2003/01/22 19:55:07 simon $
 # Sets up environment variables for development of the Stairwell demo (on
 # tiamat).
  
@@ -8,4 +8,5 @@ export TCL=/usr/lib
 export TCL_VERSION=8.3
 export BC=bc-devel
 export BUILD=/home/simon/cf/.build
+export BUILD_DIR=/home/simon/cf/.build
 export DEVEL=YES
