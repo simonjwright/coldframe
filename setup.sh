6c1a25e474154a70c71a467ba9ece766ebab92ea
Simon Wright <simon@pushface.org>
2003-01-26 17:22:28 +0000

    Added ADA_PROJECT_PATH.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
-# $Id: setup.sh,v 86e556e75755 2003/01/22 19:55:07 simon $
-# Sets up environment variables for development of the Stairwell demo (on
-# tiamat).
- 
-export TOP=/home/simon
-export TASH=tash832a
-export TCL=/usr/lib
-export TCL_VERSION=8.3
-export BC=bc-devel
+# $Id: setup.sh,v 611d1c1c658a 2003/01/26 17:22:28 simon $
+# Sets up environment variables for development of the Stairwell demo
+# (on tiamat).
+
+export ADA_PROJECT_PATH=/home/simon/cf
+export BC=${BC:bc-devel}
 export BUILD=/home/simon/cf/.build
 export BUILD_DIR=/home/simon/cf/.build
 export DEVEL=YES
+export TASH=tash832a
+export TCL=/usr/lib
+export TCL_VERSION=8.3
+export TOP=/home/simon
