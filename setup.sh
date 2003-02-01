a365a6928fd40da58b724df766fc261a01ef8858
Simon Wright <simon@pushface.org>
2003-02-01 13:06:19 +0000

    Corrected default substitution of BC.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-# $Id: setup.sh,v 611d1c1c658a 2003/01/26 17:22:28 simon $
+# $Id: setup.sh,v 5208b87b7abb 2003/02/01 13:06:19 simon $
 # Sets up environment variables for development of the Stairwell demo
 # (on tiamat).
 
 export ADA_PROJECT_PATH=/home/simon/cf
-export BC=${BC:bc-devel}
+export BC=${BC:-bc-devel}
 export BUILD=/home/simon/cf/.build
 export BUILD_DIR=/home/simon/cf/.build
 export DEVEL=YES
