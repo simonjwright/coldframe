09c9b97df914cc2c87145830691a10b5be0585f1
Simon Wright <simon@pushface.org>
2003-02-07 05:51:00 +0000

    Aded GPL copyright.

--- Digital_IO.impl/digital_io-hci-set_input.adb
+++ Digital_IO.impl/digital_io-hci-set_input.adb
@@ -1,4 +1,21 @@
---  $Id: digital_io-hci-set_input.adb,v c837ef8247ad 2003/01/07 20:10:08 simon $
+--  Copyright (C) Simon Wright <simon@pushface.org>
+
+--  This package is free software; you can redistribute it and/or
+--  modify it under terms of the GNU General Public License as
+--  published by the Free Software Foundation; either version 2, or
+--  (at your option) any later version. This package is distributed in
+--  the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+--  even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+--  PARTICULAR PURPOSE. See the GNU General Public License for more
+--  details. You should have received a copy of the GNU General Public
+--  License distributed with this package; see file COPYING.  If not,
+--  write to the Free Software Foundation, 59 Temple Place - Suite
+--  330, Boston, MA 02111-1307, USA.
+
+--  $RCSfile: digital_io-hci-set_input.adb,v $
+--  $Revision: 25594980cdc0 $
+--  $Date: 2003/02/07 05:51:00 $
+--  $Author: simon $
 
 --  Sets the specified input signal to the given value.
 
