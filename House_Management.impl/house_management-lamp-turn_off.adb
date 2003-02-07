fc2d8d64d3fe8ec8a0ecb0b68367fab67c540b0f
Simon Wright <simon@pushface.org>
2003-02-07 05:55:57 +0000

    Added GPL copyright.

--- House_Management.impl/house_management-lamp-turn_off.adb
+++ House_Management.impl/house_management-lamp-turn_off.adb
@@ -1,4 +1,21 @@
---  $Id: house_management-lamp-turn_off.adb,v c837ef8247ad 2003/01/07 20:10:08 simon $
+--  Copyright (C) Simon Wright <simon@pushface.org>
+
+--  This package is free software; you can redistribute it and/or
+--  modify it under terms of the GNU General Public License as
+--  published by the Free Software Foundation; either version 2, or
+--  (at your option) any later version. This package is distributed in
+--  the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+--  even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+--  PARTICULAR PURPOSE. See the GNU General Public License for more
+--  details. You should have received a copy of the GNU General Public
+--  License distributed with this package; see file COPYING.  If not,
+--  write to the Free Software Foundation, 59 Temple Place - Suite
+--  330, Boston, MA 02111-1307, USA.
+
+--  $RCSfile: house_management-lamp-turn_off.adb,v $
+--  $Revision: 0f7eda971a8e $
+--  $Date: 2003/02/07 05:55:57 $
+--  $Author: simon $
 
 --  This state entry action turns off the associated signal via
 --  Digital IO.
