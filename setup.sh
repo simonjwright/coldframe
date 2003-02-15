14c1177c6ca207a1bc3ec950358319f1f60482d4
Simon Wright <simon@pushface.org>
2003-02-15 16:17:21 +0000

    Added timing. Use $HOME.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
-# $Id: setup.sh,v 5208b87b7abb 2003/02/01 13:06:19 simon $
+# $Id: setup.sh,v 4c09432124cf 2003/02/15 16:17:21 simon $
 # Sets up environment variables for development of the Stairwell demo
 # (on tiamat).
 
-export ADA_PROJECT_PATH=/home/simon/cf
+export ADA_PROJECT_PATH=$HOME/cf:$HOME/timing
 export BC=${BC:-bc-devel}
-export BUILD=/home/simon/cf/.build
+export BUILD=$HOME/cf/.build
 export BUILD_DIR=/home/simon/cf/.build
 export DEVEL=YES
 export TASH=tash832a
 export TCL=/usr/lib
 export TCL_VERSION=8.3
-export TOP=/home/simon
+export TOP=$HOME
