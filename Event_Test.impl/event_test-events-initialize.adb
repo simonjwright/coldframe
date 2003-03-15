2d4b2b35b2271e5845dc586736a23e731a03bf68
Simon Wright <simon@pushface.org>
2003-03-15 22:47:32 +0000

    Removed dependency on Debugging (which has gone).

--- Event_Test.impl/event_test-events-initialize.adb
+++ Event_Test.impl/event_test-events-initialize.adb
@@ -1,8 +1,6 @@
---  with ColdFrame.Project.Events.Standard.Test;
-with Debugging;
+with ColdFrame.Project.Events.Standard.Test;
 separate (Event_Test.Events)
 procedure Initialize is
 begin
---     Dispatcher := new ColdFrame.Project.Events.Standard.Test.Event_Queue;
-   Dispatcher := new Debugging.Event_Queue;
+   Dispatcher := new ColdFrame.Project.Events.Standard.Test.Event_Queue;
 end Initialize;
