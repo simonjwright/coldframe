564940ef40d64ff765d067b939f502879bebaaf4
Simon Wright <simon@pushface.org>
2003-03-22 17:44:45 +0000

    Removed now-unnecessary constraint.

--- States.impl/states-events-initialize.adb
+++ States.impl/states-events-initialize.adb
@@ -2,6 +2,5 @@ with ColdFrame.Project.Events.Standard.Debug;
 separate (States.Events)
 procedure Initialize is
 begin
-   Dispatcher := new ColdFrame.Project.Events.Standard.Debug.Event_Queue
-                       (Start_Started => True);
+   Dispatcher := new ColdFrame.Project.Events.Standard.Debug.Event_Queue;
 end Initialize;
