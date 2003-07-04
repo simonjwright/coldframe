a8643d8b3c139abf958fc2333a7d82402ac169c1
Simon Wright <simon@pushface.org>
2003-07-04 18:23:03 +0000

    Continues.

--- cf-banner.el
+++ cf-banner.el
@@ -1,4 +1,4 @@
-;;; $Id: cf-banner.el,v 97a59a968357 2003/07/03 21:42:16 simon $
+;;; $Id: cf-banner.el,v 24c412839626 2003/07/04 18:23:03 simon $
 ;;; This GNU Emacs lisp file is part of ColdFrame.
 ;;;
 ;;; It removes banner comment at the top of Ada source files,
@@ -6,7 +6,7 @@
 ;;; revision control.
 
 (defun cf-banner ()
-  "Replace ColdFrame's initial "edit this" banner by a customized one.
+  "Replace ColdFrame's initial 'edit this' banner by a customized one.
 This version is for use with PVCS."
   (interactive "*")
   (save-excursion
@@ -18,7 +18,7 @@ This version is for use with PVCS."
 	(re-search-forward "^--\\(-\\)+$"
 			   (save-excursion (end-of-line) (point)))
 	(forward-char)
-	(search-forward "--  Automatically generated: edit this!  --"
+	(re-search-forward "^--  Automatically generated: \\(\\(edit this!\\)\\|\\(should not need editing\\)\\)  --$"
 			(save-excursion (end-of-line) (point)))
 	(forward-char)
 	(re-search-forward "^--\\(-\\)+$"
@@ -27,23 +27,11 @@ This version is for use with PVCS."
 	(delete-region (point-min) (point))
 	; banner start
 	(insert
-	 "------------------------------------------------------------------------")
+	 "------------------------------------------------------------------------------")
 	(newline)
 	; --  <last two directories on path>
 	(insert "--  ")
-	(let ((d (char-to-string directory-sep-char))
-	      (start (point))
-	      (end))
-	  (insert (buffer-file-name))
-	  (setq end (point))
-	  (re-search-backward d)
-	  (delete-region end (point))
-	  (re-search-backward d)
-	  (re-search-backward d)
-	  (forward-char)
-	  (delete-region start (point))
-	  (end-of-line)
-	  (insert d))
+	(cf--make-dir-spec)
 	(newline)
 	; PM is the filename subsitution
 	(insert "--  %PM%")
@@ -53,8 +41,26 @@ This version is for use with PVCS."
 	(newline)
 	; banner end
 	(insert
-	 "------------------------------------------------------------------------")
+	 "------------------------------------------------------------------------------")
 	(newline)))))
+
+(defun cf--make-dir-spec ()
+  "Insert a representation of the current file's location in the buffer.
+Uses the last two directory name components."
+  (let ((d (char-to-string directory-sep-char)))
+    (narrow-to-region (point) (point))
+    (insert (buffer-file-name))
+    (goto-char (point-min))
+    (while (search-forward d nil t)
+      (replace-match "/" nil t))
+    (re-search-backward "/")
+    (delete-region (point-max) (+ (point) 1))
+    (re-search-backward "/")
+    (re-search-backward "/")
+    (forward-char)
+    (delete-region (point-min) (point))
+    (end-of-line)
+    (widen)))
 
 (provide 'cf-banner)
 
