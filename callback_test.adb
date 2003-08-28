94c2f56c6a6dc32339e878a527ead8d7b0396345
Simon Wright <simon@pushface.org>
2003-08-28 21:10:52 +0000

    Include tests for exceptions in callbacks.

--- callback_test.adb
+++ callback_test.adb
@@ -1,14 +1,17 @@
---  $Id: callback_test.adb,v b42a9fa631be 2003/02/02 18:35:13 simon $
+--  $Id: callback_test.adb,v 3400fba22cc4 2003/08/28 21:10:52 simon $
 
+with Ada.Exceptions;
 with ColdFrame.Callbacks;
 with GNAT.IO; use GNAT.IO;
 
 procedure Callback_Test is
 
-   package Callbacks is new ColdFrame.Callbacks (String);
+   package String_Callback is new ColdFrame.Callbacks (String);
 
    procedure P (S : String);
    procedure Q (S : String);
+   procedure Constraint_Err (S : String);
+   procedure Program_Err (S : String);
 
    procedure P (S : String) is
    begin
@@ -20,48 +23,71 @@ procedure Callback_Test is
       Put_Line ("Q called with """ & S & """");
    end Q;
 
+   procedure Constraint_Err (S : String) is
+   begin
+      raise Constraint_Error;
+   end Constraint_Err;
+
+   procedure Program_Err (S : String) is
+   begin
+      raise Program_Error;
+   end Program_Err;
+
 begin
 
-   Callbacks.Call_Callbacks ("1");
+   String_Callback.Call_Callbacks ("1");
 
-   Callbacks.Register (P'Access);
+   String_Callback.Register (P'Access);
 
-   Callbacks.Call_Callbacks ("2");
+   String_Callback.Call_Callbacks ("2");
 
-   Callbacks.Register (P'Access);
+   String_Callback.Register (P'Access);
 
-   Callbacks.Call_Callbacks ("3");
+   String_Callback.Call_Callbacks ("3");
 
-   Callbacks.Register (Q'Access);
+   String_Callback.Register (Q'Access);
 
-   Callbacks.Call_Callbacks ("4");
+   String_Callback.Call_Callbacks ("4");
 
-   Callbacks.Clear;
+   String_Callback.Clear;
 
-   Callbacks.Call_Callbacks ("5");
+   String_Callback.Call_Callbacks ("5");
 
-   Callbacks.Register (P'Access);
+   String_Callback.Register (P'Access);
 
-   Callbacks.Call_Callbacks ("6");
+   String_Callback.Call_Callbacks ("6");
 
-   Callbacks.Register (P'Access);
+   String_Callback.Register (P'Access);
 
-   Callbacks.Call_Callbacks ("7");
+   String_Callback.Call_Callbacks ("7");
 
-   Callbacks.Register (Q'Access);
+   String_Callback.Register (Q'Access);
 
-   Callbacks.Call_Callbacks ("8");
+   String_Callback.Call_Callbacks ("8");
 
-   Callbacks.Deregister (P'Access);
+   String_Callback.Deregister (P'Access);
 
-   Callbacks.Call_Callbacks ("9");
+   String_Callback.Call_Callbacks ("9");
 
-   Callbacks.Deregister (Q'Access);
+   String_Callback.Deregister (Q'Access);
 
-   Callbacks.Call_Callbacks ("10");
+   String_Callback.Call_Callbacks ("10");
 
-   Callbacks.Clear;
+   String_Callback.Clear;
 
-   Callbacks.Call_Callbacks ("11");
+   String_Callback.Call_Callbacks ("11");
+
+   String_Callback.Clear;
+
+   begin
+      String_Callback.Register (Q'Access);
+      String_Callback.Register (Constraint_Err'Access);
+      String_Callback.Register (P'Access);
+      String_Callback.Register (Program_Err'Access);
+      String_Callback.Call_Callbacks ("12");
+   exception
+      when E : others =>
+         Put_Line (Ada.Exceptions.Exception_Information (E));
+   end;
 
 end Callback_Test;
