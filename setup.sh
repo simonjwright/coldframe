0ae8e502bdf834b536625c10afe34281269aba63
Simon Wright <simon@pushface.org>
2003-11-08 16:36:25 +0000

    timing moves to high_resolution_timing. Added statistics.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-# $Id: setup.sh,v 8590d689a774 2003/09/10 04:54:01 simon $
+# $Id: setup.sh,v cbc9b02db43f 2003/11/08 16:36:25 simon $
 # Sets up environment variables for development of the Stairwell demo
 # (on tiamat).
 
-export ADA_PROJECT_PATH=$HOME/cf:$HOME/timing
+export ADA_PROJECT_PATH=$HOME/cf:$HOME/high_resolution_timing:$HOME/statistics
 export BC=${BC:-bc}
 export BUILD=$HOME/cf/.build
 export BUILD_DIR=/home/simon/cf/.build
