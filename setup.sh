24fa8ab3c758491ac0ff174f21f6490ddda5f527
Simon Wright <simon@pushface.org>
2003-11-15 05:47:37 +0000

    Put case exceptions the right way round (I think -- new ones go in ~/.emacs_case_exceptions).

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
-# $Id: setup.sh,v 1d847b9ff4bb 2003/11/14 06:13:35 simon $
+# $Id: setup.sh,v 09da305d3a40 2003/11/15 05:47:37 simon $
 # Sets up environment variables for development of the Stairwell demo
 # (on tiamat).
 
-export ADA_PROJECT_PATH=$HOME/cf:$HOME/high_resolution_timing:$HOME/statistics
+export ADA_PROJECT_PATH=$HOME/cf
 export BC=${BC:-bc}
 export BUILD=$HOME/cf/.build
 export BUILD_DIR=/home/simon/cf/.build
-export CASE_EXCEPTIONS=$HOME/cf/emacs_case_exceptions:\
-$HOME/.emacs_case_exceptions
+# I think the first writable one is where new entries are added ..
+export CASE_EXCEPTIONS=\
+$HOME/.emacs_case_exceptions:\
+$HOME/cf/emacs_case_exceptions
 export DEVEL=YES
 export TASH=tash832a
 export TCL=/usr/lib
