0edda93bf22bf0f237f934d9993d387cc609a3d7
Simon Wright <simon@pushface.org>
2004-03-13 18:39:15 +0000

    Report if we don't get aborted.

--- Regressions.impl/regressions-event_holder-t.adb
+++ Regressions.impl/regressions-event_holder-t.adb
@@ -1,5 +1,7 @@
+with Ada.Text_IO; use Ada.Text_IO;
 separate (Regressions.Event_Holder)
 task body T is
 begin
-   delay 10.0;
+   delay 5.0;
+   Put_Line (Standard_Error, "Event_Holder.T passed its delay");
 end T;
--- Regressions.impl/regressions-find_active_singleton-t.adb
+++ Regressions.impl/regressions-find_active_singleton-t.adb
@@ -1,5 +1,7 @@
+with Ada.Text_IO; use Ada.Text_IO;
 separate (Regressions.Find_Active_Singleton)
 task body T is
 begin
-   delay 100.0;
+   delay 5.0;
+   Put_Line (Standard_Error, "Find_Active_Singleton.T passed its delay");
 end T;
