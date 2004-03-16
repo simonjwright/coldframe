1a9b2525c89c5f72581c6d010570b55bd4b043c2
Simon Wright <simon@pushface.org>
2004-03-16 17:43:56 +0000

    Added completion transition tests.

--- Event_Test.test/event_test-suite.adb
+++ Event_Test.test/event_test-suite.adb
@@ -2,6 +2,7 @@ with Event_Test.Test_Engine;
 with Event_Test.Test_Instance;
 with Event_Test.Test_Singleton_Instance;
 with Event_Test.Test_Class;
+with Event_Test.Test_Completion_Transitions;
 
 function Event_Test.Suite return AUnit.Test_Suites.Access_Test_Suite is
    use AUnit.Test_Suites;
@@ -11,5 +12,6 @@ begin
    Add_Test (Result, new Test_Instance.Test_Case);
    Add_Test (Result, new Test_Singleton_Instance.Test_Case);
    Add_Test (Result, new Test_Class.Test_Case);
+   Add_Test (Result, new Test_Completion_Transitions.Test_Case);
    return Result;
 end Event_Test.Suite;
