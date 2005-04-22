3787271f399eabaca36b310eb05f9fbb1e80d099
Simon Wright <simon@pushface.org>
2005-04-22 05:30:39 +0000

    Added "debugger view" checks.

--- test_storage_pools.adb
+++ test_storage_pools.adb
@@ -13,19 +13,22 @@
 --  330, Boston, MA 02111-1307, USA.
 
 --  $RCSfile: test_storage_pools.adb,v $
---  $Revision: ebabd218849a $
---  $Date: 2004/05/20 05:04:24 $
---  $Author: simon $
+--  $Revision: 784987b4d752 $
+--  $Date: 2005/04/22 05:30:39 $
+--  $Author: simonjwright $
 
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Unchecked_Deallocation;
 with ColdFrame.Project.Storage_Pools;
+with Interfaces;
 with System.Storage_Elements;
 
 procedure Test_Storage_Pools is
 
 begin
+
+   Put_Line ("Bounded_Pool tests.");
 
    for Size in System.Storage_Elements.Storage_Offset'(0) .. 17 loop
 
@@ -79,6 +82,9 @@ begin
 
    end loop;
 
+   New_Line;
+   Put_Line ("Unbounded Pool tests.");
+
    for Size in System.Storage_Elements.Storage_Offset'(0) .. 17 loop
 
       declare
@@ -124,5 +130,77 @@ begin
       end;
 
    end loop;
+
+   --  When looking at data with the debugger you tend to see fills
+   --  best when printing inb hex (print/x), and of course the result
+   --  is endian-dependent. Make sure that the data looks good:
+
+   New_Line;
+   Put_Line ("Debugger view (Bounded_Pool).");
+
+   declare
+
+      type T is new Interfaces.Unsigned_64;
+
+      package T_IO is new Modular_IO (T);
+
+      T_Pool : ColdFrame.Project.Storage_Pools.Bounded_Pool
+        (Pool_Size => 128,
+         Elmt_Size => T'Max_Size_In_Storage_Elements,
+         Alignment => T'Alignment);
+      pragma Warnings (Off, T_Pool);
+
+      type T_P is access T;
+      for T_P'Storage_Pool use T_Pool;
+      procedure Free is new Ada.Unchecked_Deallocation (T, T_P);
+
+      P : T_P;
+      Dangling_P : T_P;
+
+   begin
+
+      P := new T;
+
+      T_IO.Put (P.all, Base => 16);
+      New_Line;
+
+      Dangling_P := P;
+      P.all := 0;
+      Free (P);
+      T_IO.Put (Dangling_P.all, Base => 16);
+      New_Line;
+
+   end;
+
+   New_Line;
+   Put_Line ("Debugger view (unbounded Pool).");
+
+   declare
+
+      type T is new Interfaces.Unsigned_64;
+
+      package T_IO is new Modular_IO (T);
+
+      type T_P is access T;
+      for T_P'Storage_Pool use ColdFrame.Project.Storage_Pools.Pool;
+      procedure Free is new Ada.Unchecked_Deallocation (T, T_P);
+
+      P : T_P;
+      Dangling_P : T_P;
+
+   begin
+
+      P := new T;
+
+      T_IO.Put (P.all, Base => 16);
+      New_Line;
+
+      Dangling_P := P;
+      P.all := 0;
+      Free (P);
+      T_IO.Put (Dangling_P.all, Base => 16);
+      New_Line;
+
+   end;
 
 end Test_Storage_Pools;
