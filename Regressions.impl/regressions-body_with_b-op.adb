417dd34588bc2e4421ffed5a3bc29adfb93eb79c
Simon Wright <simon@pushface.org>
2005-06-02 19:38:01 +0000

    Shuold have used Body_With_A; this sort of with not yet implemented.

--- Regressions.impl/regressions-body_with_b-op.adb
+++ Regressions.impl/regressions-body_with_b-op.adb
@@ -2,8 +2,8 @@ separate (Regressions.Body_With_B)
 procedure Op
   (This : Handle) is
    pragma Warnings (Off, This);
-   A : Body_With_B.Handle;
-   pragma Warnings (Off, A);
+--     A : Body_With_A.Handle;
+--     pragma Warnings (Off, A);
 begin
    null;
 end Op;
