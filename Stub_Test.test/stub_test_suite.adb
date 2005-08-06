2e969623e723d038b9f4f5c6d735424022a29f01
Simon Wright <simon@pushface.org>
2005-08-06 17:48:32 +0000

    Included tests for bad names and overriding.

--- Stub_Test.test/stub_test_suite.adb
+++ Stub_Test.test/stub_test_suite.adb
@@ -13,8 +13,8 @@
 --  330, Boston, MA 02111-1307, USA.
 
 --  $RCSfile: stub_test_suite.adb,v $
---  $Revision: e64ad9ec2d98 $
---  $Date: 2005/07/12 21:07:20 $
+--  $Revision: 3656e70ef5ae $
+--  $Date: 2005/08/06 17:48:32 $
 --  $Author: simonjwright $
 
 with AUnit.Test_Cases.Registration; use AUnit.Test_Cases.Registration;
@@ -53,6 +53,120 @@ package body Stub_Test_Suite is
    is new ColdFrame.Stubs.Get_Input_Value (Stub_Test.Fixed_Message);
    function Get_Variable_Message
    is new ColdFrame.Stubs.Get_Input_Value (Stub_Test.Variable_Message);
+
+
+   procedure Bad_Names
+     (C : in out AUnit.Test_Cases.Test_Case'Class);
+   procedure Bad_Names
+     (C : in out AUnit.Test_Cases.Test_Case'Class) is
+      pragma Warnings (Off, C);
+      I : Integer;
+      pragma Warnings (Off, I);
+   begin
+      begin
+         Set_Integer ("Stub_Test.Public.Get_Values", "return", 42, 2);
+         Assert (False,
+                 "should have raised exception (a)");
+      exception
+         when ColdFrame.Stubs.No_Subprogram => null;
+      end;
+      begin
+         Set_Integer ("Stub_Test.Public.Get_Value", "result", 42, 2);
+         Assert (False,
+                 "should have raised exception (b)");
+      exception
+         when ColdFrame.Stubs.No_Parameter => null;
+      end;
+      begin
+         I := Get_Integer ("Stub_Test.Public.Set_Values", "I", 1);
+         Assert (False,
+                 "should have raised exception (c)");
+      exception
+         when ColdFrame.Stubs.No_Subprogram => null;
+      end;
+      begin
+         I := Get_Integer ("Stub_Test.Public.Set_Value", "J", 1);
+         Assert (False,
+                 "should have raised exception (d)");
+      exception
+         when ColdFrame.Stubs.No_Parameter => null;
+      end;
+      begin
+         ColdFrame.Stubs.Set_Exception ("Stub_Test.Public.Get_Values",
+                                        Constraint_Error'Identity,
+                                        3);
+         Assert (False,
+                 "should have raised exception (e)");
+      exception
+         when ColdFrame.Stubs.No_Subprogram => null;
+      end;
+      begin
+         I := ColdFrame.Stubs.Number_Of_Calls ("Stub_Test.Public.Get_Values");
+         Assert (False,
+                 "should have raised exception (f)");
+      exception
+         when ColdFrame.Stubs.No_Subprogram => null;
+      end;
+   end Bad_Names;
+
+
+   procedure Overridden_Values
+     (C : in out AUnit.Test_Cases.Test_Case'Class);
+   procedure Overridden_Values
+     (C : in out AUnit.Test_Cases.Test_Case'Class) is
+      pragma Warnings (Off, C);
+      E, F, G, H : exception;
+      I : Integer;
+   begin
+      Set_Integer ("Stub_Test.Public.Get_Value", "return", 42, 1);
+      Set_Integer ("Stub_Test.Public.Get_Value", "return", 43, 2);
+      ColdFrame.Stubs.Set_Exception
+        ("Stub_Test.Public.Get_Value", E'Identity, 2);
+      begin
+         Set_Integer ("Stub_Test.Public.Get_Value", "return", 142, 1);
+         Assert (False,
+                 "should have raised exception (a)");
+      exception
+         when ColdFrame.Stubs.Already_Set => null;
+      end;
+      begin
+         ColdFrame.Stubs.Set_Exception
+           ("Stub_Test.Public.Get_Value", F'Identity, 2);
+         Assert (False,
+                 "should have raised exception (b)");
+      exception
+         when ColdFrame.Stubs.Already_Set => null;
+      end;
+      begin
+         Set_Integer
+           ("Stub_Test.Public.Get_Value", "return", 144, 3, Override => True);
+         Assert (False,
+                 "should have raised exception (c)");
+      exception
+         when ColdFrame.Stubs.Not_Already_Set => null;
+      end;
+      begin
+         ColdFrame.Stubs.Set_Exception
+           ("Stub_Test.Public.Get_Value", G'Identity, 1, Override => True);
+         Assert (False,
+                 "should have raised exception (d)");
+      exception
+         when ColdFrame.Stubs.Not_Already_Set => null;
+      end;
+      Set_Integer
+        ("Stub_Test.Public.Get_Value", "return", 242, 1, Override => True);
+      ColdFrame.Stubs.Set_Exception
+        ("Stub_Test.Public.Get_Value", H'Identity, 2, Override => True);
+      I := Stub_Test.Public.Get_Value;
+      Assert (I = 242, "value not overridden; got " & I'Img);
+      begin
+         I := Stub_Test.Public.Get_Value;
+         Assert (False,
+                 "should have raised exception (e)");
+      exception
+         when H => null;
+      end;
+   end Overridden_Values;
 
 
    procedure Missing_Values
@@ -343,6 +457,14 @@ package body Stub_Test_Suite is
 
    procedure Register_Tests (C : in out Case_1) is
    begin
+      Register_Routine
+        (C,
+         Bad_Names'Access,
+         "bad names");
+      Register_Routine
+        (C,
+         Overridden_Values'Access,
+         "overridden values");
       Register_Routine
         (C,
          Missing_Values'Access,
