5af4636fc4c91275f6b2df263fca57ed726a71b0
Simon Wright <simon@pushface.org>
2005-08-07 15:41:01 +0000

    Added Test_Queue.

--- Event_Test.test/event_test-suite.adb
+++ Event_Test.test/event_test-suite.adb
@@ -2,6 +2,7 @@ with Event_Test.Test_Class;
 with Event_Test.Test_Completion_Transitions;
 with Event_Test.Test_Engine;
 with Event_Test.Test_Instance;
+with Event_Test.Test_Queue;
 with Event_Test.Test_Singleton_Instance;
 with Event_Test.Test_Timer_Task_Teardown;
 
@@ -9,6 +10,7 @@ function Event_Test.Suite return AUnit.Test_Suites.Access_Test_Suite is
    use AUnit.Test_Suites;
    Result : constant Access_Test_Suite := new Test_Suite;
 begin
+   Add_Test (Result, new Test_Queue.Test_Case);
    Add_Test (Result, new Test_Engine.Test_Case);
    Add_Test (Result, new Test_Instance.Test_Case);
    Add_Test (Result, new Test_Singleton_Instance.Test_Case);
