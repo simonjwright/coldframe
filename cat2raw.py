401e773bba993147431e54282e3ed6b9623f529e
Simon Wright <simon@pushface.org>
2005-12-31 18:09:52 +0000

    Allow CURDIR to have a different capitalization.

--- cat2raw.py
+++ cat2raw.py
@@ -14,7 +14,7 @@
 #  write to the Free Software Foundation, 59 Temple Place - Suite
 #  330, Boston, MA 02111-1307, USA.
 
-# $Id: cat2raw.py,v 52988b23a553 2005/10/19 19:14:41 simonjwright $
+# $Id: cat2raw.py,v 112225fee980 2005/12/31 18:09:52 simonjwright $
 
 # Reads a Rose .cat file and converts it to ColdFrame .raw format.
 
@@ -270,7 +270,11 @@ class Domain(Base):
 	dirname = os.path.dirname(path)
 	if not dirname:
 	    dirname = '.'
-	filename = re.sub(r'^\$CURDIR', dirname, self.file_name)
+	    """Rose doesn't require that CURDIR be all upper-case, so some folk
+	    may mix the case."""
+	filename = re.sub(re.compile(r'^\$CURDIR', re.IGNORECASE),
+			  dirname,
+			  self.file_name)
 	filename = re.sub(r'\\\\', '/', filename)
 	sys.stderr.write('  included file \"%s\"\n' % filename)
 	lexer = lex.lex()
@@ -312,7 +316,7 @@ class Domain(Base):
     def emit_contents(self, to):
 	yr, mo, dy, hr, mn, s, wd, yd, dst = time.localtime(time.time())
 	self.emit_single_element('extractor',
-				 'cat2raw.py: $Revision: 52988b23a553 $',
+				 'cat2raw.py: $Revision: 112225fee980 $',
 				 to)
 	to.write('<date>\n')
 	self.emit_single_element('year', yr, to)
@@ -828,7 +832,7 @@ def t_error(t):
 def main():
     
     def usage():
-	sys.stderr.write('%s $Revision: 52988b23a553 $\n' % sys.argv[0])
+	sys.stderr.write('%s $Revision: 112225fee980 $\n' % sys.argv[0])
 	sys.stderr.write('usage: cat2raw.py [flags] [input cat file]\n')
 	sys.stderr.write('flags:\n')
 	sys.stderr.write('-h, --help:              '
