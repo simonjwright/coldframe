5837fe5bba4d05400a759cbe7ca05d14a28e4b34
Simon Wright <simon@pushface.org>
2006-03-18 17:32:51 +0000

    First version.

---
+++ Unit_Testing.impl/unit_testing-normal-post_c.adb
@@ -0,0 +1,11 @@
+with ColdFrame.Project.Events;
+with Unit_Testing.Events;
+
+separate (Unit_Testing.Normal)
+procedure Post_C is
+begin
+   ColdFrame.Project.Events.Set (S,
+                                 On => Events.Dispatcher,
+                                 To_Fire => new C,
+                                 After => 1.0);
+end Post_C;
