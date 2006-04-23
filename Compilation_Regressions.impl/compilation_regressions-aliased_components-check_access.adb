3c87638b5c4dd07611e537c584418489aca34902
Simon Wright <simon@pushface.org>
2006-04-23 16:03:19 +0000

    Check record type with aliased component.

--- Compilation_Regressions.impl/compilation_regressions-aliased_components-check_access.adb
+++ Compilation_Regressions.impl/compilation_regressions-aliased_components-check_access.adb
@@ -4,6 +4,7 @@ separate (Compilation_Regressions.Aliased_Components)
 procedure Check_Access
   (This : Handle) is
    type Integer_P is access all Integer;
+   R : Record_With_Aliased_Component := (I => 42);
    B : Aliasable_Boolean_P;
    I : Integer_P;
    pragma Unreferenced (B, I);
@@ -12,4 +13,5 @@ begin
    I := This.Ins_I'Access;
    B := Cls_B'Access;
    I := Cls_I'Access;
+   I := R.I'Access;
 end Check_Access;
