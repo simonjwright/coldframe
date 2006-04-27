3c38037469e99119e78ba0d2dccd7aac3529271e
Simon Wright <simon@pushface.org>
2006-04-27 05:16:40 +0000

    Partial implementation.

--- Recording.impl/recording-recorder-initialize.adb
+++ Recording.impl/recording-recorder-initialize.adb
@@ -1,13 +1,18 @@
---------------------------------------------
---  Automatically generated:  edit this!  --
---  PLEASE DELETE THIS BANNER AFTERWARDS  --
---------------------------------------------
-
 --  Sets up the Socket and the Buffer.
 
 separate (Recording.Recorder)
 procedure Initialize is
-   Unimplemented : exception;
 begin
-   raise Unimplemented;
+
+   --  OK to initialize Sockets more than once, not OK not to
+   --  initialize at all!
+   GNAT.Sockets.Initialize;
+
+   --  Need to create the Socket.
+   --  GNAT.Create_Socket ()
+
+   --  The Buffer's capacity would probably be adaptable via a runtime
+   --  parameter.
+   This.Buff.Initialize;
+
 end Initialize;
