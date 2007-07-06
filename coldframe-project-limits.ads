7d21bf154b3d972abdfc47683b5dc02db3a9bca3
Simon Wright <simon@pushface.org>
2007-07-06 04:55:08 +0000

    First version.

---
+++ coldframe-project-limits.ads
@@ -0,0 +1,25 @@
+--  Copyright (C) Simon Wright <simon@pushface.org>
+
+--  This unit is free software; you can redistribute it and/or modify
+--  it as you wish. This unit is distributed in the hope that it will
+--  be useful, but WITHOUT ANY WARRANTY; without even the implied
+--  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+--  $RCSfile: coldframe-project-limits.ads,v $
+--  $Revision: 228b749dc5b6 $
+--  $Date: 2007/07/06 04:55:08 $
+--  $Author: simonjwright $
+
+--  This package specifies trigger limits used by the implementation.
+
+package ColdFrame.Project.Limits is
+
+   subtype Milliseconds is Integer;
+
+   Monitor_Report_Trigger : Milliseconds := 1_000;
+   pragma Export (C, Monitor_Report_Trigger, "monitor_report_trigger");
+   --  If a Monitoring event queue fails to return to idle after this
+   --  length of continous event processing, it will log a trace of
+   --  the events involved.
+
+end ColdFrame.Project.Limits;
