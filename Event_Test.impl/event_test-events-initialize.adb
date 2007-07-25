28c76c0725c18ed0e4026ad2fafa88e0ab22b76a
Simon Wright <simon@pushface.org>
2007-07-25 06:18:55 +0000

    Included test for monitoring Event Queue.

--- Event_Test.impl/event_test-events-initialize.adb
+++ Event_Test.impl/event_test-events-initialize.adb
@@ -1,6 +1,10 @@
-with ColdFrame.Project.Events.Standard.Test;
+with ColdFrame.Project.Events.Monitoring.Test;
 separate (Event_Test.Events)
 procedure Initialize is
+   subtype Milliseconds is Integer;
+   Monitor_Report_Trigger_Ms : Milliseconds;
+   pragma Import (Ada, Monitor_Report_Trigger_Ms, "monitor_report_trigger");
 begin
-   Dispatcher := new ColdFrame.Project.Events.Standard.Test.Event_Queue;
+   Monitor_Report_Trigger_Ms := 1;
+   Dispatcher := new ColdFrame.Project.Events.Monitoring.Test.Event_Queue;
 end Initialize;
