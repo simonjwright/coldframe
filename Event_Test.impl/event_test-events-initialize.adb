99c86c79356e9038805131820a511ed709e13384
Simon Wright <simon@pushface.org>
2007-07-28 06:17:34 +0000

    Leave trigger set to default (1 s).

--- Event_Test.impl/event_test-events-initialize.adb
+++ Event_Test.impl/event_test-events-initialize.adb
@@ -1,10 +1,6 @@
 with ColdFrame.Project.Events.Monitoring.Test;
 separate (Event_Test.Events)
 procedure Initialize is
-   subtype Milliseconds is Integer;
-   Monitor_Report_Trigger_Ms : Milliseconds;
-   pragma Import (Ada, Monitor_Report_Trigger_Ms, "monitor_report_trigger");
 begin
-   Monitor_Report_Trigger_Ms := 1;
    Dispatcher := new ColdFrame.Project.Events.Monitoring.Test.Event_Queue;
 end Initialize;
