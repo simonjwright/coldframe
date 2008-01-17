132c60ed129a4611d3eeb21eb408db58646c5c67
Simon Wright <simon@pushface.org>
2008-01-17 23:50:14 +0000

    This Instance doesn't need to be a child of the ColdFrame Instance_Base.

--- Top_Half.impl/top_half-abstract_bottom_half.ads
+++ Top_Half.impl/top_half-abstract_bottom_half.ads
@@ -1,9 +1,6 @@
-with ColdFrame.Instances;
-
 package Top_Half.Abstract_Bottom_Half is
 
-   type Instance
-      is abstract new ColdFrame.Instances.Instance_Base with null record;
+   type Instance is abstract tagged null record;
    type Handle is access all Instance'Class;
 
    procedure Perform (This : Instance; X : Integer) is abstract;
