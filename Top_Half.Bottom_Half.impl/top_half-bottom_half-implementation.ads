659deec02a75f3cb19acd3502b947819ad3c1a39
Simon Wright <simon@pushface.org>
2008-01-21 21:38:13 +0000

    Don't need Handle.

--- Top_Half.Bottom_Half.impl/top_half-bottom_half-implementation.ads
+++ Top_Half.Bottom_Half.impl/top_half-bottom_half-implementation.ads
@@ -4,7 +4,6 @@ private package Top_Half.Bottom_Half.Implementation is
 
    type Instance
    is new Top_Half.Abstract_Bottom_Half.Instance with null record;
-   type Handle is access Instance;
 
    procedure Perform (This : Instance; X : Integer);
 
