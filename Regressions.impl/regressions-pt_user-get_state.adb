8cf17d339d91da1bd6ef5b10a029c49b90ed8a0a
Simon Wright <simon@pushface.org>
2008-07-07 20:49:03 +0000

    Get_H no longer allowed in Ada 05.

--- Regressions.impl/regressions-pt_user-get_state.adb
+++ Regressions.impl/regressions-pt_user-get_state.adb
@@ -5,5 +5,5 @@ function Get_State
   return Boolean is
    pragma Assert (Domain_Initialized, "Regressions not initialized");
 begin
-   return PT_Owner.Get_H.Get_State;
+   return PT_Owner.Get_H_Access.Get_State;
 end Get_State;
