ecc80154935ff82f4c6dea6d08d32c5902b33388
Simon Wright <simon@pushface.org>
2011-05-03 22:33:47 +0100

    Updated more copyright statements. Uncommented the 'with' of Compilation_Regressions.Singleton_Assoc in test.

--- test/Compilation_Regressions.impl/compilation_regressions-subunits.adb
+++ test/Compilation_Regressions.impl/compilation_regressions-subunits.adb
@@ -1,8 +1,17 @@
---  $RCSfile$
---  $Revision$
---  $Date$
---  $Author$
---
+--  Copyright (C) Simon Wright <simon@pushface.org>
+
+--  This package is free software; you can redistribute it and/or
+--  modify it under terms of the GNU General Public License as
+--  published by the Free Software Foundation; either version 2, or
+--  (at your option) any later version. This package is distributed in
+--  the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+--  even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+--  PARTICULAR PURPOSE. See the GNU General Public License for more
+--  details. You should have received a copy of the GNU General Public
+--  License distributed with this package; see file COPYING.  If not,
+--  write to the Free Software Foundation, 59 Temple Place - Suite
+--  330, Boston, MA 02111-1307, USA.
+
 --  Regression tests for ColdFrame.
 --
 --  The with'ed units only have to compile.
@@ -49,8 +58,7 @@ with Compilation_Regressions.Serializable;
 with Compilation_Regressions.Service;
 with Compilation_Regressions.Service_History;
 with Compilation_Regressions.Short_Modular_Ident;
---  Doesn't work yet (23.iv.06).
---  with Compilation_Regressions.Singleton_Assoc;
+with Compilation_Regressions.Singleton_Assoc;
 with Compilation_Regressions.Singleton_With_Attributes;
 with Compilation_Regressions.Singleton_With_Referential_Attribute;
 with Compilation_Regressions.Singleton_Without_Attributes;
