bd89d4cf60bc522abb41b50d63dab0dad9225173
Simon Wright <simon@pushface.org>
2012-02-22 15:21:30 +0000

    Included the States example.

--- examples/States.impl/states-monitor-receive.adb
+++ examples/States.impl/states-monitor-receive.adb
@@ -1,9 +1,29 @@
+--  Copyright (C) Simon Wright <simon@pushface.org>
+
+--  This package is free software; you can redistribute it and/or
+--  modify it under terms of the GNU General Public License as
+--  published by the Free Software Foundation; either version 2, or
+--  (at your option) any later version. This package is distributed in
+--  the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+--  even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+--  PARTICULAR PURPOSE. See the GNU General Public License for more
+--  details. You should have received a copy of the GNU General Public
+--  License distributed with this package; see file COPYING. If not,
+--  write to the Free Software Foundation, 59 Temple Place - Suite
+--  330, Boston, MA 02111-1307, USA.
+
+--  This is a class event handler.
+
+--  It receives an external message indicating that a tick has
+--  occurred for a Device. Generates a Heartbeat event for the Device
+--  concerned.
+
 with States.Events;
 separate (States.Monitor)
 procedure Receive
-  (M : Tick) is
+  (Tick_For : Device) is
 begin
    ColdFrame.Project.Events.Post
-     (The_Event => new Monitor.Heartbeat (Find ((Dev => M.Payload))),
+     (The_Event => new Monitor.Heartbeat (Find ((Dev => Tick_For))),
       On => Events.Dispatcher);
 end Receive;
--- examples/States.impl/states-t.adb
+++ examples/States.impl/states-t.adb
@@ -1,14 +1,28 @@
+--  Copyright (C) Simon Wright <simon@pushface.org>
+
+--  This package is free software; you can redistribute it and/or
+--  modify it under terms of the GNU General Public License as
+--  published by the Free Software Foundation; either version 2, or
+--  (at your option) any later version. This package is distributed in
+--  the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+--  even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+--  PARTICULAR PURPOSE. See the GNU General Public License for more
+--  details. You should have received a copy of the GNU General Public
+--  License distributed with this package; see file COPYING. If not,
+--  write to the Free Software Foundation, 59 Temple Place - Suite
+--  330, Boston, MA 02111-1307, USA.
+
 with GNAT.IO; use GNAT.IO;
 with ColdFrame.Exceptions.Traceback;
 pragma Warnings (Off, ColdFrame.Exceptions.Traceback);
-with ColdFrame.Project.Events;
+with ColdFrame.Project.Events.Standard.Trace;
 with States.Initialize;
 with States.Events;
 with States.Monitor;
 procedure States.T is
    E : ColdFrame.Project.Events.Event_P;
 begin
-   Initialize;
+   Initialize (new ColdFrame.Project.Events.Standard.Trace.Event_Queue);
    for N in 1 .. 10 loop
       delay 1.0;
       E := new Monitor.Tick;
