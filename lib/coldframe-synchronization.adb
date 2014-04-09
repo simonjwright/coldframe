13644dd31936be5263761e513fb4b3b5ffbb3bda
Simon Wright <simon@pushface.org>
2014-04-09 11:36:27 +0100

    Copied Synchronization package from the Booch Components.
      * lib/coldframe-synchronization.adb,
        lib/coldframe-synchronization.ads: new.

---
+++ lib/coldframe-synchronization.adb
@@ -0,0 +1,248 @@
+--  Copyright Simon Wright <simon@pushface.org>
+
+--  This package is free software; you can redistribute it and/or
+--  modify it under terms of the GNU General Public License as
+--  published by the Free Software Foundation; either version 2, or
+--  (at your option) any later version. This package is distributed in
+--  the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+--  even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+--  PARTICULAR PURPOSE. See the GNU General Public License for more
+--  details. You should have received a copy of the GNU General Public
+--  License distributed with this package; see file COPYING.  If not,
+--  write to the Free Software Foundation, 59 Temple Place - Suite
+--  330, Boston, MA 02111-1307, USA.
+
+--  As a special exception, if other files instantiate generics from
+--  this unit, or you link this unit with other files to produce an
+--  executable, this unit does not by itself cause the resulting
+--  executable to be covered by the GNU General Public License.  This
+--  exception does not however invalidate any other reasons why the
+--  executable file might be covered by the GNU Public License.
+
+--  This unit has been copied with minor modifications from the Booch
+--  Components, where it was BC.Support.Synchronization.
+
+--  $RCSfile: coldframe-synchronization.adb,v $
+--  $Revision: 022e59ccfb79 $
+--  $Date: 2014/04/09 10:36:27 $
+--  $Author: simonjwright $
+
+package body ColdFrame.Synchronization is
+
+   --  Semaphore  --
+
+   protected body Semaphore_Type is
+
+      entry Seize when not Seized is
+      begin
+         Seized := True;
+      end Seize;
+
+      procedure Release is
+      begin
+         Seized := False;
+      end Release;
+
+      function None_Pending return Boolean is
+      begin
+         --  See LRM 4.1.4(14).
+         return Semaphore_Type.Seize'Count = 0;
+      end None_Pending;
+
+   end Semaphore_Type;
+
+   procedure Seize (The_Semaphore : in out Semaphore) is
+   begin
+      The_Semaphore.S.Seize;
+   end Seize;
+
+   procedure Release (The_Semaphore : in out Semaphore) is
+   begin
+      The_Semaphore.S.Release;
+   end Release;
+
+   function None_Pending (On_The_Semaphore : Semaphore) return Boolean is
+   begin
+      return On_The_Semaphore.S.None_Pending;
+   end None_Pending;
+
+   --  Recursive_Semaphore  --
+
+   protected body Recursive_Semaphore_Type is
+
+      entry Seize when True is
+         use type Ada.Task_Identification.Task_Id;
+      begin
+         --  Why do I say Recursive_Semaphore_Type.Seize rather than
+         --  just Seize?
+         --
+         --  Jean-Pascal Cozic <Jean-Pascal.Cozic@ingsud.dga.fr> says:
+         --  You have to write Recursive_Semaphore_Type.Seize'Caller
+         --  because there are two homographs Seize in the scope (the
+         --  entry and the procedure). Using the context, there is no
+         --  ambiguity because Caller deals with a prefix that denotes
+         --  an entry_declaration, but the compiler cannot use context
+         --  in this case.
+         --
+         --  See LRM 4.1.4(14).
+         if Owner = Recursive_Semaphore_Type.Seize'Caller then
+            Count := Count + 1;
+         else
+            requeue Waiting with abort;
+         end if;
+      end Seize;
+
+      procedure Release is
+      begin
+         Count := Count - 1;
+      end Release;
+
+      function None_Pending return Boolean is
+      begin
+         return Waiting'Count = 0;
+      end None_Pending;
+
+      entry Waiting when Count = 0 is
+      begin
+         Owner := Waiting'Caller;
+         Count := 1;
+      end Waiting;
+
+   end Recursive_Semaphore_Type;
+
+   procedure Seize (The_Semaphore : in out Recursive_Semaphore) is
+   begin
+      The_Semaphore.S.Seize;
+   end Seize;
+
+   procedure Release (The_Semaphore : in out Recursive_Semaphore) is
+   begin
+      The_Semaphore.S.Release;
+   end Release;
+
+   function None_Pending
+     (On_The_Semaphore : Recursive_Semaphore) return Boolean is
+   begin
+      return On_The_Semaphore.S.None_Pending;
+   end None_Pending;
+
+   --  Single_Monitor  --
+
+   procedure Seize_For_Reading (The_Monitor : in out Single_Monitor) is
+   begin
+      The_Monitor.The_Semaphore.Seize;
+   end Seize_For_Reading;
+
+   procedure Seize_For_Writing (The_Monitor : in out Single_Monitor) is
+   begin
+      The_Monitor.The_Semaphore.Seize;
+   end Seize_For_Writing;
+
+   procedure Release_From_Reading (The_Monitor : in out Single_Monitor) is
+   begin
+      The_Monitor.The_Semaphore.Release;
+   end Release_From_Reading;
+
+   procedure Release_From_Writing (The_Monitor : in out Single_Monitor) is
+   begin
+      The_Monitor.The_Semaphore.Release;
+   end Release_From_Writing;
+
+   --  Multiple_Monitor  --
+
+   protected body Monitor_Type is
+
+      entry Seize (Kind : in Seize_Kind)
+      when Waiting_To_Write'Count = 0 and then not Writing is
+      begin
+         case Kind is
+            when For_Reading =>
+               Reader_Count := Reader_Count + 1;
+            when For_Writing =>
+               requeue Waiting_To_Write with abort;
+         end case;
+      end Seize;
+
+      procedure Release_From_Reading is
+      begin
+         Reader_Count := Reader_Count - 1;
+      end Release_From_Reading;
+
+      procedure Release_From_Writing is
+      begin
+         Writing := False;
+      end Release_From_Writing;
+
+      entry Waiting_To_Write when Reader_Count = 0 is
+      begin
+         Writing := True;
+      end Waiting_To_Write;
+
+   end Monitor_Type;
+
+   procedure Seize_For_Reading (The_Monitor : in out Multiple_Monitor) is
+   begin
+      The_Monitor.M.Seize (Kind => For_Reading);
+   end Seize_For_Reading;
+
+   procedure Seize_For_Writing (The_Monitor : in out Multiple_Monitor) is
+   begin
+      The_Monitor.M.Seize (Kind => For_Writing);
+   end Seize_For_Writing;
+
+   procedure Release_From_Reading (The_Monitor : in out Multiple_Monitor) is
+   begin
+      The_Monitor.M.Release_From_Reading;
+   end Release_From_Reading;
+
+   procedure Release_From_Writing (The_Monitor : in out Multiple_Monitor) is
+   begin
+      The_Monitor.M.Release_From_Writing;
+   end Release_From_Writing;
+
+   --  Lock  --
+
+   procedure Initialize (The_Lock : in out Lock) is
+   begin
+      The_Lock.Using.Seize;
+   end Initialize;
+
+   procedure Finalize (The_Lock : in out Lock) is
+   begin
+      if not The_Lock.Finalized then
+         The_Lock.Finalized := True;
+         The_Lock.Using.Release;
+      end if;
+   end Finalize;
+
+   --  Read_Lock  --
+
+   procedure Initialize (The_Lock : in out Read_Lock) is
+   begin
+      The_Lock.Using.Seize_For_Reading;
+   end Initialize;
+
+   procedure Finalize (The_Lock : in out Read_Lock) is
+   begin
+      if not The_Lock.Finalized then
+         The_Lock.Finalized := True;
+         The_Lock.Using.Release_From_Reading;
+      end if;
+   end Finalize;
+
+   --  Write_Lock  --
+
+   procedure Initialize (The_Lock : in out Write_Lock) is
+   begin
+      The_Lock.Using.Seize_For_Writing;
+   end Initialize;
+
+   procedure Finalize (The_Lock : in out Write_Lock) is
+   begin
+      if not The_Lock.Finalized then
+         The_Lock.Finalized := True;
+         The_Lock.Using.Release_From_Writing;
+      end if;
+   end Finalize;
+
+end ColdFrame.Synchronization;
---
+++ lib/coldframe-synchronization.ads
@@ -0,0 +1,192 @@
+--  Copyright Simon Wright <simon@pushface.org>
+
+--  This package is free software; you can redistribute it and/or
+--  modify it under terms of the GNU General Public License as
+--  published by the Free Software Foundation; either version 2, or
+--  (at your option) any later version. This package is distributed in
+--  the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+--  even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+--  PARTICULAR PURPOSE. See the GNU General Public License for more
+--  details. You should have received a copy of the GNU General Public
+--  License distributed with this package; see file COPYING.  If not,
+--  write to the Free Software Foundation, 59 Temple Place - Suite
+--  330, Boston, MA 02111-1307, USA.
+
+--  As a special exception, if other files instantiate generics from
+--  this unit, or you link this unit with other files to produce an
+--  executable, this unit does not by itself cause the resulting
+--  executable to be covered by the GNU General Public License.  This
+--  exception does not however invalidate any other reasons why the
+--  executable file might be covered by the GNU Public License.
+
+--  This unit has been copied from the Booch Components, where it was
+--  BC.Support.Synchronization.
+
+--  $RCSfile: coldframe-synchronization.ads,v $
+--  $Revision: 022e59ccfb79 $
+--  $Date: 2014/04/09 10:36:27 $
+--  $Author: simonjwright $
+
+private with Ada.Finalization;
+private with Ada.Task_Identification;
+
+package ColdFrame.Synchronization is
+
+   pragma Elaborate_Body;
+
+   --  Semaphores provide for mutual exclusion.
+   type Semaphore_Base is abstract tagged limited private;
+   procedure Seize (The_Semaphore : in out Semaphore_Base) is abstract;
+   procedure Release (The_Semaphore : in out Semaphore_Base) is abstract;
+   function None_Pending (On_The_Semaphore : Semaphore_Base) return Boolean
+      is abstract;
+
+
+   --  A Semaphore is like a standard POSIX mutex.
+   type Semaphore is new Semaphore_Base with private;
+   procedure Seize (The_Semaphore : in out Semaphore);
+   procedure Release (The_Semaphore : in out Semaphore);
+   function None_Pending (On_The_Semaphore : Semaphore) return Boolean;
+
+
+   --  A Recursive_Semaphore is like a POSIX recursive mutex; once
+   --  Seized by a task, that task can Seize again; other tasks are
+   --  blocked until the owning task has Released the semaphore as
+   --  many times as it Seized it.
+   type Recursive_Semaphore is new Semaphore_Base with private;
+   procedure Seize (The_Semaphore : in out Recursive_Semaphore);
+   procedure Release (The_Semaphore : in out Recursive_Semaphore);
+   function None_Pending
+     (On_The_Semaphore : Recursive_Semaphore) return Boolean;
+
+
+   --  Monitors support Locks.
+   type Monitor_Base is abstract tagged limited private;
+   procedure Seize_For_Reading (The_Monitor : in out Monitor_Base)
+      is abstract;
+   procedure Seize_For_Writing (The_Monitor : in out Monitor_Base)
+      is abstract;
+   procedure Release_From_Reading (The_Monitor : in out Monitor_Base)
+      is abstract;
+   procedure Release_From_Writing (The_Monitor : in out Monitor_Base)
+      is abstract;
+
+
+   --  Single_Monitors allow one task at a time to have access, be it
+   --  for reading or writing.
+   type Single_Monitor is new Monitor_Base with private;
+   procedure Seize_For_Reading (The_Monitor : in out Single_Monitor);
+   procedure Seize_For_Writing (The_Monitor : in out Single_Monitor);
+   procedure Release_From_Reading (The_Monitor : in out Single_Monitor);
+   procedure Release_From_Writing (The_Monitor : in out Single_Monitor);
+
+
+   --  Multiple_Monitors allow multiple readers; however, when a
+   --  writer owns the monitor it has exclusive access.
+   type Multiple_Monitor is new Monitor_Base with private;
+   procedure Seize_For_Reading (The_Monitor : in out Multiple_Monitor);
+   procedure Seize_For_Writing (The_Monitor : in out Multiple_Monitor);
+   procedure Release_From_Reading (The_Monitor : in out Multiple_Monitor);
+   procedure Release_From_Writing (The_Monitor : in out Multiple_Monitor);
+
+
+   --  A Lock is designed to provide "locking by declaration".
+   --    declare
+   --      L : Lock (Some_Monitor'Access);
+   --    begin
+   --      -- the monitor is locked
+   --    end;
+   --    -- the monitor is unlocked as L is finalized, even if an exception
+   --    -- occurs
+
+   type Lock_Base is abstract tagged limited private;
+
+   --  A simple Lock provides mutual exclusion
+   type Lock (Using : access Semaphore_Base'Class)
+   is new Lock_Base with private;
+
+   --  Read_ and Write_ Locks support multiple reader/single writer
+   --  access provided the given Monitor supports it; otherwise they
+   --  merely provide mutual exclusion.
+   type Read_Lock (Using : access Monitor_Base'Class)
+   is new Lock_Base with private;
+
+   type Write_Lock (Using : access Monitor_Base'Class)
+   is new Lock_Base with private;
+
+private
+
+   type Semaphore_Base is abstract tagged limited null record;
+
+   protected type Semaphore_Type is
+      entry Seize;
+      procedure Release;
+      function None_Pending return Boolean;
+   private
+      Seized : Boolean := False;
+   end Semaphore_Type;
+
+   type Semaphore is new Semaphore_Base with record
+      S : Semaphore_Type;
+   end record;
+
+   protected type Recursive_Semaphore_Type is
+      entry Seize;
+      procedure Release;
+      function None_Pending return Boolean;
+   private
+      entry Waiting;
+      Owner : Ada.Task_Identification.Task_Id;
+      Count : Natural := 0;
+   end Recursive_Semaphore_Type;
+
+   type Recursive_Semaphore is new Semaphore_Base with record
+      S : Recursive_Semaphore_Type;
+   end record;
+
+   type Monitor_Base is abstract tagged limited null record;
+
+   type Single_Monitor is new Monitor_Base with record
+      The_Semaphore : Recursive_Semaphore;
+   end record;
+
+   --  Monitor_Type is due to Matthew Heaney <matthew_heaney@acm.org>.
+   --  The Booch C++ version was inoperative, at least in sjw's translation.
+
+   type Seize_Kind is (For_Reading, For_Writing);
+
+   protected type Monitor_Type is
+      entry Seize (Kind : Seize_Kind);
+      procedure Release_From_Reading;
+      procedure Release_From_Writing;
+   private
+      entry Waiting_To_Write;
+      Reader_Count : Natural := 0;
+      Writing : Boolean := False;
+   end Monitor_Type;
+
+   type Multiple_Monitor is new Monitor_Base with record
+      M : Monitor_Type;
+   end record;
+
+   type Lock_Base
+   is abstract new Ada.Finalization.Limited_Controlled with record
+      Finalized : Boolean := False;
+   end record;
+
+   type Lock (Using : access Semaphore_Base'Class)
+   is new Lock_Base with null record;
+   procedure Initialize (The_Lock : in out Lock);
+   procedure Finalize (The_Lock : in out Lock);
+
+   type Read_Lock (Using : access Monitor_Base'Class)
+   is new Lock_Base with null record;
+   procedure Initialize (The_Lock : in out Read_Lock);
+   procedure Finalize (The_Lock : in out Read_Lock);
+
+   type Write_Lock (Using : access Monitor_Base'Class)
+   is new Lock_Base with null record;
+   procedure Initialize (The_Lock : in out Write_Lock);
+   procedure Finalize (The_Lock : in out Write_Lock);
+
+end ColdFrame.Synchronization;
