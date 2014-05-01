bc440002e0c7e123b4c3b4e45ac6e95d0fd8adec
Simon Wright <simon@pushface.org>
2014-05-01 15:47:49 +0100

    Interrupt_Handling demo withed obsolete package;
      * examples/Interrupt_Handling.impl/interrupt_handling-events-initialize.adb:
        withed but didn't use ColdFrame.Project.Events.Standard.Debug,
        which has been replaced by ...Trace, but wasn't used in any case!
        Just with the Standard package.

--- examples/Interrupt_Handling.impl/interrupt_handling-events-initialize.adb
+++ examples/Interrupt_Handling.impl/interrupt_handling-events-initialize.adb
@@ -1,10 +1,7 @@
-with ColdFrame.Project.Events.Standard.Debug;
+with ColdFrame.Project.Events.Standard;
 
 separate (Interrupt_Handling.Events)
 procedure Initialize is
 begin
-
---     Dispatcher := new ColdFrame.Project.Events.Standard.Debug.Event_Queue;
    Dispatcher := new ColdFrame.Project.Events.Standard.Event_Queue;
-
 end Initialize;
