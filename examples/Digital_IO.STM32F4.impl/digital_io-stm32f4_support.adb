79227f39b83a7c1f9838ae76faddb65023e873f5
Simon Wright <simon@pushface.org>
2015-04-01 14:35:24 +0100

    STM32F4 example continued. Initial attempts at bit/partword indexable access support.
      * examples/Digital_IO.STM32F4.impl/digital_io-stm32f4_support.ads
        (IRQ_Handler): removed.
        (Bits_1, Bits_2 etc, Bits_16x1 etc, U16_To_B1 etc): new.
    
      * examples/Digital_IO.STM32F4.impl/digital_io-stm32f4_support.adb
        (EXTI15_10_IRQ_T and EXTI15_10_IRQ): removed.
        (EXTI_Configuration): new.
        (EXTI_Handler): converted from EXTI15_10_IRQ_T. Protected object,
          not type. Handles all of the IRQs.
        (Receive_Button_Interrupt): made task, not task type.
        (Receive_EXTI15_10_IRQ): removed.
        (Configure_Inputs); uses EXTI_Configuration.
        (Configure_Outputs): likewise.
        (Set): likewise.
        (Get): implemented.

--- examples/Digital_IO.STM32F4.impl/digital_io-stm32f4_support.adb
+++ examples/Digital_IO.STM32F4.impl/digital_io-stm32f4_support.adb
@@ -12,70 +12,208 @@
 
 with Ada.Interrupts.Names;
 with Digital_IO.Input_Signal_State_Callback;
-with Interfaces;
 with System;
 with stm32f429xx_h; use stm32f429xx_h;
 
 package body Digital_IO.STM32F4_Support is
 
-   protected type EXTI15_10_IRQ_T
+   package EXTI_Configuration is
+
+      type EXTI_Line is mod 16;
+
+      type Used_Input_Signal (Valid : Boolean := False) is record
+         case Valid is
+            when False => null;
+            when True  => Signal : Input_Signal;
+         end case;
+      end record;
+      Line_To_Input_Signal :
+        constant array (EXTI_Line) of Used_Input_Signal :=
+        (8      => (True, 0),
+         11     => (True, 1),
+         12     => (True, 2),
+         2      => (True, 3),
+         --  4      => (True, 4),
+         others => <>);
+
+      type Volatile_GPIO_TypeDef is new GPIO_TypeDef with Volatile;
+      type GPIO_Access is access all Volatile_GPIO_TypeDef;
+
+      type GPIO_Index is (A, B, C, D, E, F, G, H, I, J, K)
+      with
+        Warnings => Off;
+      --  Note, 'Pos is the bit number in AHB1ENR, and the number to
+      --  set in EXTICR to associate the Line with the GPIO.
+
+      GPIOs : constant array (GPIO_Index) of GPIO_Access :=
+        (Volatile_GPIO_TypeDef (GPIOA)'Unrestricted_Access,
+         Volatile_GPIO_TypeDef (GPIOB)'Unrestricted_Access,
+         Volatile_GPIO_TypeDef (GPIOC)'Unrestricted_Access,
+         Volatile_GPIO_TypeDef (GPIOD)'Unrestricted_Access,
+         Volatile_GPIO_TypeDef (GPIOE)'Unrestricted_Access,
+         Volatile_GPIO_TypeDef (GPIOF)'Unrestricted_Access,
+         Volatile_GPIO_TypeDef (GPIOG)'Unrestricted_Access,
+         Volatile_GPIO_TypeDef (GPIOH)'Unrestricted_Access,
+         Volatile_GPIO_TypeDef (GPIOI)'Unrestricted_Access,
+         Volatile_GPIO_TypeDef (GPIOJ)'Unrestricted_Access,
+         Volatile_GPIO_TypeDef (GPIOK)'Unrestricted_Access);
+      --  Sorry about the Unrestricted_Access, only way I found to
+      --  work round the rules.
+
+      type Used_Line (Valid : Boolean := False) is record
+         case Valid is
+            when False =>
+               null;
+            when True  =>
+               GPIO : GPIO_Index;
+               Line : EXTI_Line;
+         end case;
+      end record;
+
+      Input_Signal_To_Line : constant array (Input_Signal) of Used_Line :=
+        (0      => (True, C, 8),
+         1      => (True, C, 11),
+         2      => (True, C, 12),
+         3      => (True, D, 2),
+         --  4      => (True, D, 4),
+         others => <>);
+
+      Output_Signal_To_Line : constant array (Output_Signal) of Used_Line :=
+        (0      => (True, A, 5),
+         1      => (True, C, 3),
+         2      => (True, F, 6),
+         3      => (True, G, 2),
+         others => <>);
+
+   end EXTI_Configuration;
+   use EXTI_Configuration;
+
+   protected EXTI_Handler
    with
      Interrupt_Priority => System.Interrupt_Priority'First
-   is new IRQ_Handler with
-      pragma Warnings (Off, "*not referenced*");
-      overriding
-      entry Button_Pressed
-        (B : out Digital_IO_Support.Input_Signal);
-      pragma Warnings (On, "*not referenced*");
+   is
+      entry Button_Pressed (B : out Interfaces.Unsigned_32);
    private
-      Triggered : Boolean := False;
-      Last_Button_Pressed : Digital_IO_Support.Input_Signal;
-      procedure Handler;
-      pragma Attach_Handler (Handler, Ada.Interrupts.Names.EXTI15_10_IRQ);
-   end EXTI15_10_IRQ_T;
+      Triggered        : Boolean := False;
 
-   EXTI15_10_IRQ : aliased EXTI15_10_IRQ_T;
+      Last_Read_Values : Interfaces.Unsigned_32 := 0;
+      --  We accumulate interrupt indications as they arrive, and
+      --  clear when retreived by the caller of Button_Pressed.
 
-   task type Receive_Button_Interrupt
-     (IRQ : access IRQ_Handler'Class)
+      procedure EXTI0_IRQ_Handler;
+      pragma Attach_Handler (EXTI0_IRQ_Handler,
+                               Ada.Interrupts.Names.EXTI0_IRQ);
+      procedure EXTI1_IRQ_Handler;
+      pragma Attach_Handler (EXTI1_IRQ_Handler,
+                               Ada.Interrupts.Names.EXTI1_IRQ);
+      procedure EXTI2_IRQ_Handler;
+      pragma Attach_Handler (EXTI2_IRQ_Handler,
+                               Ada.Interrupts.Names.EXTI2_IRQ);
+      procedure EXTI3_IRQ_Handler;
+      pragma Attach_Handler (EXTI3_IRQ_Handler,
+                               Ada.Interrupts.Names.EXTI3_IRQ);
+      procedure EXTI4_IRQ_Handler;
+      pragma Attach_Handler (EXTI4_IRQ_Handler,
+                               Ada.Interrupts.Names.EXTI4_IRQ);
+      procedure EXTI9_5_IRQ_Handler;
+      pragma Attach_Handler (EXTI9_5_IRQ_Handler,
+                               Ada.Interrupts.Names.EXTI9_5_IRQ);
+      procedure EXTI15_10_IRQ_Handler;
+      pragma Attach_Handler (EXTI15_10_IRQ_Handler,
+                               Ada.Interrupts.Names.EXTI15_10_IRQ);
+   end EXTI_Handler;
+
+   task Receive_Button_Interrupt
    is
    end Receive_Button_Interrupt;
 
-   Receive_EXTI15_10_IRQ :
-     Receive_Button_Interrupt (IRQ => EXTI15_10_IRQ'Access);
-
-   protected body EXTI15_10_IRQ_T is
-      entry Button_Pressed
-        (B : out Digital_IO_Support.Input_Signal)
+   protected body EXTI_Handler is
+      entry Button_Pressed (B : out Interfaces.Unsigned_32)
         when Triggered is
       begin
-         B := Last_Button_Pressed;
+         B := Last_Read_Values;
+         Last_Read_Values := 0;
          Triggered := False;
       end Button_Pressed;
 
-      procedure Handler is
+      procedure EXTI0_IRQ_Handler is
+         Read_Value : constant Interfaces.Unsigned_32 := EXTI.PR;
          use type Interfaces.Unsigned_32;
-         PR : Interfaces.Unsigned_32;
       begin
-         PR := EXTI.PR;
-         if (PR and EXTI_PR_PR11) /= 0 then
-            Last_Button_Pressed :=  Digital_IO_Support.Input_Signal'First;
-            Triggered := True;
-            EXTI.PR := EXTI_PR_PR11;  -- clear the interrupt by *writing* 1
-         else
-            raise Program_Error with "unexpected interrupt";
-         end if;
-      end Handler;
-   end EXTI15_10_IRQ_T;
+         EXTI.PR := Read_Value;
+         Triggered := True;
+         Last_Read_Values := Last_Read_Values or Read_Value;
+      end EXTI0_IRQ_Handler;
+
+      procedure EXTI1_IRQ_Handler is
+         Read_Value : constant Interfaces.Unsigned_32 := EXTI.PR;
+         use type Interfaces.Unsigned_32;
+      begin
+         EXTI.PR := Read_Value;
+         Triggered := True;
+         Last_Read_Values := Last_Read_Values or Read_Value;
+      end EXTI1_IRQ_Handler;
+
+      procedure EXTI2_IRQ_Handler is
+         Read_Value : constant Interfaces.Unsigned_32 := EXTI.PR;
+         use type Interfaces.Unsigned_32;
+      begin
+         EXTI.PR := Read_Value;
+         Triggered := True;
+         Last_Read_Values := Last_Read_Values or Read_Value;
+      end EXTI2_IRQ_Handler;
+
+      procedure EXTI3_IRQ_Handler is
+         Read_Value : constant Interfaces.Unsigned_32 := EXTI.PR;
+         use type Interfaces.Unsigned_32;
+      begin
+         EXTI.PR := Read_Value;
+         Triggered := True;
+         Last_Read_Values := Last_Read_Values or Read_Value;
+      end EXTI3_IRQ_Handler;
+
+      procedure EXTI4_IRQ_Handler is
+         Read_Value : constant Interfaces.Unsigned_32 := EXTI.PR;
+         use type Interfaces.Unsigned_32;
+      begin
+         EXTI.PR := Read_Value;
+         Triggered := True;
+         Last_Read_Values := Last_Read_Values or Read_Value;
+      end EXTI4_IRQ_Handler;
+
+      procedure EXTI9_5_IRQ_Handler is
+         Read_Value : constant Interfaces.Unsigned_32 := EXTI.PR;
+         use type Interfaces.Unsigned_32;
+      begin
+         EXTI.PR := Read_Value;
+         Triggered := True;
+         Last_Read_Values := Last_Read_Values or Read_Value;
+      end EXTI9_5_IRQ_Handler;
+
+      procedure EXTI15_10_IRQ_Handler is
+         Read_Value : constant Interfaces.Unsigned_32 := EXTI.PR;
+         use type Interfaces.Unsigned_32;
+      begin
+         EXTI.PR := Read_Value;
+         Triggered := True;
+         Last_Read_Values := Last_Read_Values or Read_Value;
+      end EXTI15_10_IRQ_Handler;
+   end EXTI_Handler;
 
    task body Receive_Button_Interrupt is
-      Last_Button_Pressed : Digital_IO_Support.Input_Signal;
+      Last_Read_Values : Interfaces.Unsigned_32;
+      Bits : Bits_32x1;
    begin
       loop
-         IRQ.Button_Pressed (Last_Button_Pressed);
-         Input_Signal_State_Callback.Call_Callbacks
-           ((S => Integer (Last_Button_Pressed),
-             State => True));
+         EXTI_Handler.Button_Pressed (Last_Read_Values);
+         Bits := U32_To_B1 (Last_Read_Values);
+         for Line in EXTI_Line loop
+            if Bits (Integer (Line)) /= 0 then
+               Input_Signal_State_Callback.Call_Callbacks
+                 ((S     => Line_To_Input_Signal (Line).Signal,
+                   State => True));
+            end if;
+         end loop;
       end loop;
    end Receive_Button_Interrupt;
 
@@ -92,98 +230,148 @@ package body Digital_IO.STM32F4_Support is
    procedure Configure_Inputs is
       use type Interfaces.Unsigned_32;
    begin
-      --  First, we need to enable GPIOC's clock.
-      RCC.AHB1ENR := RCC.AHB1ENR or RCC_AHB1ENR_GPIOCEN;
+      for J in Input_Signal'Range loop
+         if Input_Signal_To_Line (J).Valid then
+            --  Enable the GPIO's clock.
+            declare
+               AHB1ENR : Bits_32x1 := U32_To_B1 (RCC.AHB1ENR);
+            begin
+               AHB1ENR (GPIO_Index'Pos (Input_Signal_To_Line (J).GPIO)) := 1;
+               RCC.AHB1ENR := B1_To_U32 (AHB1ENR);
+            end;
 
-      --  Don't need to configure bit 11 as an input, because that's
-      --  the default.
+            declare
+               GPIO : Volatile_GPIO_TypeDef renames
+                 GPIOs (Input_Signal_To_Line (J).GPIO).all;
+               Line : constant Natural :=
+                 Natural (Input_Signal_To_Line (J).Line);
+            begin
+               --  Don't need to configure the line as an input, because
+               --  that's the default.
 
-      --  Don't need to configure OTYPER, because the pin is an input.
+               --  Don't need to configure OTYPER, because the pin is an
+               --  input.
 
-      --  Don't need to configure OSPEEDR, because the pin is an input.
+               --  Don't need to configure OSPEEDR, because the pin is an
+               --  input.
 
-      --  Configure PUPDR for pull-up (AdaCore have no-pull?)
-      --  This means that the 'on' state is logic 0.
-      declare
-         Pull_Register : Interfaces.Unsigned_32;
-      begin
-         Pull_Register := GPIOC.PUPDR;
-         Pull_Register := Pull_Register and not GPIO_PUPDR_PUPDR11;
-         Pull_Register := Pull_Register or GPIO_PUPDR_PUPDR11_0;
-         GPIOC.PUPDR := Pull_Register;
-      end;
+               --  Configure PUPDR for pull-up.
+               --  This means that the 'on' state is logic 0.
+               declare
+                  PUPDR : Bits_32x2 := U32_To_B2 (GPIO.PUPDR);
+               begin
+                  PUPDR (Line) := 1;
+                  GPIO.PUPDR := B2_To_U32 (PUPDR);
+               end;
 
-      --  Enable SYSCFG's clock.
-      RCC.APB2ENR := RCC.APB2ENR or RCC_APB2ENR_SYSCFGEN;
+               --  Enable SYSCFG's clock.
+               RCC.APB2ENR := RCC.APB2ENR or RCC_APB2ENR_SYSCFGEN;
 
-      --  Then, configure SYSCFG_EXTICR3[12:15] (pin 11) for GPIOC.
-      declare
-         Exti_Register : Interfaces.Unsigned_32;
-      begin
-         Exti_Register := SYSCFG.EXTICR (2);  -- C indexing!
-         Exti_Register := Exti_Register and not SYSCFG_EXTICR3_EXTI11;
-         Exti_Register := Exti_Register or SYSCFG_EXTICR3_EXTI11_PC;
-         SYSCFG.EXTICR (2) := Exti_Register;
-      end;
+               --  Connect the GPIO to the interrupt Line.
+               declare
+                  Offset : constant Natural := Line / 4;
+                  Index : constant Natural := Line mod 4;
+                  EXTICR : Bits_32x4 := U32_To_B4 (SYSCFG.EXTICR (Offset))
+                    with Volatile;
+               begin
+                  EXTICR (Index) :=
+                    GPIO_Index'Pos (Input_Signal_To_Line (J).GPIO);
+                  SYSCFG.EXTICR (Offset) := B4_To_U32 (EXTICR);
+               end;
 
-      --  Then, configure EXTI on pin 11 for rising-edge (button-release).
-      EXTI.RTSR := EXTI.RTSR or EXTI_RTSR_TR11;
-      --  and enable interrupts on pin 11.
-      EXTI.IMR := EXTI.IMR or EXTI_IMR_MR11;
+               --  Then, configure EXTI on this line for rising-edge
+               --  (button-release).
+               declare
+                  RTSR : Bits_32x1 := U32_To_B1 (EXTI.RTSR)
+                    with Volatile;
+               begin
+                  RTSR (Line) := 1;
+                  EXTI.RTSR := B1_To_U32 (RTSR);
+               end;
+
+               --  and enable interrupts on the line.
+               declare
+                  IMR : Bits_32x1 := U32_To_B1 (EXTI.IMR)
+                    with Volatile;
+               begin
+                  IMR (Line) := 1;
+                  EXTI.IMR := B1_To_U32 (IMR);
+               end;
+            end;
+         end if;
+      end loop;
    end Configure_Inputs;
 
    procedure Configure_Outputs is
-      use type Interfaces.Unsigned_32;
    begin
-      --  We're going to use a pin on the right-hand side of the card;
-      --  P2/21 is PA5, GPIOA pin 5.
+      for J in Output_Signal'Range loop
+         if Output_Signal_To_Line (J).Valid then
+            --  Enable the GPIO's clock.
+            declare
+               AHB1ENR : Bits_32x1 := U32_To_B1 (RCC.AHB1ENR);
+            begin
+               AHB1ENR (GPIO_Index'Pos (Output_Signal_To_Line (J).GPIO)) := 1;
+               RCC.AHB1ENR := B1_To_U32 (AHB1ENR);
+            end;
 
-      --  First, we need to enable GPIOA's clock.
-      RCC.AHB1ENR := RCC.AHB1ENR or RCC_AHB1ENR_GPIOAEN;
+            --  Configure the line as an output.
+            declare
+               GPIO : Volatile_GPIO_TypeDef renames
+                 GPIOs (Output_Signal_To_Line (J).GPIO).all;
+               Line : constant Natural :=
+                 Natural (Output_Signal_To_Line (J).Line);
+               MODER : Bits_32x2 := U32_To_B2 (GPIO.MODER);
+            begin
+               MODER (Line) := 1;
+               GPIO.MODER := B2_To_U32 (MODER);
+            end;
 
-      --  Then, we need to configure bit 5 as an output.
-      declare
-         Mode_Register : Interfaces.Unsigned_32;
-      begin
-         Mode_Register := GPIOA.MODER;
-         Mode_Register := Mode_Register and not GPIO_MODER_MODER5;
-         Mode_Register := Mode_Register or Interfaces.Shift_Left (1, 5 * 2);
-         GPIOA.MODER := Mode_Register;
-      end;
+            --  Don't need to configure OTYPER, because the default is
+            --  push-pull.
 
-      --  Don't need to configure OTYPER, because the default is push-pull.
+            --  Don't need to configure OSPEEDR, because the default
+            --  is slow, which is (I expect) more than adequate for an
+            --  LED!
 
-      --  Don't need to configure OSPEEDR, because the default is
-      --  slow, which is (I expect) more than adequate for an LED!
-
-      --  Don't need to configure PUPDR, because the default is no
-      --  pull-up, no pull-down.
+            --  Don't need to configure PUPDR, because the default is
+            --  no pull-up, no pull-down.
+         end if;
+      end loop;
    end Configure_Outputs;
 
 
-   function Get (This : Implementation;
+   function Get (This      : Implementation;
                  For_Input : Digital_IO_Support.Input_Signal)
                 return Boolean
    is
       pragma Unreferenced (This);  -- only used for dispatching
       use type Interfaces.Unsigned_32;
+      Line : constant Natural :=
+        Natural (Input_Signal_To_Line (Input_Signal (For_Input)).Line);
+      GPIO : Volatile_GPIO_TypeDef renames
+        GPIOs (Input_Signal_To_Line (Input_Signal (For_Input)).GPIO).all;
    begin
       --  The 'on' state is logic 0, because we're pulling up.
-      return (GPIOC.IDR and GPIO_IDR_IDR_11) = 0;
+      return U32_To_B1 (GPIO.IDR)(Line) = 0;
    end Get;
 
-   procedure Set (This : Implementation;
+   procedure Set (This       : Implementation;
                   For_Output : Digital_IO_Support.Output_Signal;
-                  To : Boolean)
+                  To         : Boolean)
    is
       pragma Unreferenced (This);  -- only used for dispatching
-      use type Interfaces.Unsigned_32;
+      Line : constant Natural :=
+        Natural (Output_Signal_To_Line (Output_Signal (For_Output)).Line);
+      Bits : Bits_16x1 := (others => 0);
+      GPIO : Volatile_GPIO_TypeDef renames
+        GPIOs (Output_Signal_To_Line (Output_Signal (For_Output)).GPIO).all;
    begin
+      Bits (Line) := 1;
       case To is
          when False =>
-            GPIOA.BSRRH := Interfaces.Shift_Left (1, 5); -- off
+            GPIO.BSRRH := B1_To_U16 (Bits); -- off
          when True =>
-            GPIOA.BSRRL := Interfaces.Shift_Left (1, 5); -- on
+            GPIO.BSRRL := B1_To_U16 (Bits); -- on
       end case;
    end Set;
 
