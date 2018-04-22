95d712d7a21cc84bb39c577425eceed43dc88780
Simon Wright <simon@pushface.org>
2018-04-22 15:58:50 +0100

    Added copyright notice.
      * examples/House_Management.impl/house_management-lamp-changed.adb:
          added copyright notice.

--- examples/House_Management.impl/house_management-lamp-changed.adb
+++ examples/House_Management.impl/house_management-lamp-changed.adb
@@ -1,7 +1,16 @@
---------------------------------------------
---  Automatically generated:  edit this!  --
---  PLEASE DELETE THIS BANNER AFTERWARDS  --
---------------------------------------------
+--  Copyright (C) Simon Wright <simon@pushface.org>
+
+--  This package is free software; you can redistribute it and/or
+--  modify it under terms of the GNU General Public License as
+--  published by the Free Software Foundation; either version 2, or
+--  (at your option) any later version. This package is distributed in
+--  the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+--  even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+--  PARTICULAR PURPOSE. See the GNU General Public License for more
+--  details. You should have received a copy of the GNU General Public
+--  License distributed with this package; see file COPYING.  If not,
+--  write to the Free Software Foundation, 59 Temple Place - Suite
+--  330, Boston, MA 02111-1307, USA.
 
 --  This operation is used by a linked Button to tell the Lamp
 --  to evaluate all its linked Buttons to see if any are Set (in which
