7d27c7f95a2d757f28481b4fcba416aad8e33f2e
Simon Wright <simon@pushface.org>
2019-02-05 14:09:04 +0000

    Replaced aggregate projects with ada-mode projects. Added Secondary_Stack_Size to dispatcher instantiations.
      * examples/Simple_Buttons_Scripting.prj: new.
      * examples/arduino/simple_buttons.prj: likewise.
      * examples/microbit/simple_buttons.prj: likewise.
      * examples/stm32f4/simple_buttons.prj: likewise.
    
      * examples/arduino/simple_buttons_dispatcher.ads: added Secondary_Stack_Size.
      * examples/stm32f4/simple_buttons_dispatcher.ads: likewise.
    
      * examples/arduino/simple_buttons.gpr: updated comment.
      * examples/microbit/simple_buttons.gpr: likewise.
    
      * examples/Simple_Buttons_Scripting_Master.gpr: removed.
      * examples/arduino/simple_buttons_master.gpr: likewise.
      * examples/microbit/simple_buttons_master.gpr: likewise.
      * examples/stm32f4/simple_buttons_master.gpr: likewise.

--- examples/stm32f4/simple_buttons_dispatcher.ads
+++ examples/stm32f4/simple_buttons_dispatcher.ads
@@ -5,8 +5,9 @@ package Simple_Buttons_Dispatcher is
 
    --  The ColdFrame event queue.
    Dispatcher : aliased ColdFrame.Events.Standard.Event_Queue_Base
-     (Priority => System.Default_Priority,
-      Storage_Size => 2048,
-      Capacity => 16);
+     (Priority             => System.Default_Priority,
+      Storage_Size         => 2048,
+      Secondary_Stack_Size => 256,
+      Capacity             => 16);
 
 end Simple_Buttons_Dispatcher;
