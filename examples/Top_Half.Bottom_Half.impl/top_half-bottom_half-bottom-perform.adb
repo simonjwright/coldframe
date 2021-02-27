70076a416888aa184540cb9b319aa3f4edab391f
Simon Wright <simon@pushface.org>
2021-02-27 12:21:47 +0000

    Removed the Top_Half/Bottom_Half example code.

--- examples/Top_Half.Bottom_Half.impl/top_half-bottom_half-bottom-perform.adb
+++
@@ -1,8 +0,0 @@
-with Ada.Text_IO; use Ada.Text_IO;
-
-separate (Top_Half.Bottom_Half.Bottom)
-procedure Perform
-  (X : Integer) is
-begin
-   Put_Line ("Top_Half.Bottom_Half.Bottom.Perform called with " & X'Img);
-end Perform;
