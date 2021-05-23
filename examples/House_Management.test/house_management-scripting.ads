ea46516e35dedd59ef10227abf86b4c08c2d1c17
Simon Wright <simon@pushface.org>
2021-05-23 18:42:01 +0100

    Reduce priority of scripting main program.
      * examples/House_Management.test/house_management-scripting.ads: new.
          Priority set to default - 1.

---
+++ examples/House_Management.test/house_management-scripting.ads
@@ -0,0 +1,11 @@
+--  Copyright (C) Simon Wright <simon@pushface.org>
+
+--  This unit is free software; you can redistribute it and/or modify
+--  it as you wish. This unit is distributed in the hope that it will
+--  be useful, but WITHOUT ANY WARRANTY; without even the implied
+--  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+with System;
+
+procedure House_Management.Scripting
+with Priority => System.Default_Priority - 1;
