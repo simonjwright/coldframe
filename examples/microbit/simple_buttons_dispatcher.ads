7f5ad8cf1098192f21026076799f85c0f2e3272a
Simon Wright <simon@pushface.org>
2023-02-15 17:07:34 +0000

    Work on Ravenscar examples.
      * examples/microbit/Makefile (clean): just remove the only .gpr.
      * examples/microbit/alire.toml (environment): new; sets TARGET, RTS,
          COLDFRAME_BUILD, PLATFORM_BUILD,ADL_BUILD, ADL_BUILD_CHECKS.
        (actions, pre-build): makes Simple_Buttons.gen, Digital_IO.gen.
        (actions, post-build): builds simple_buttons.hex.
        (depends-on): gnat_arm_elf (can't be used, because BSS too large;
          won't be used, because the RTS is just microbit, not embedded-microbit)/
      * examples/microbit/simple_buttons.gpr (Builder): removed -O2.
        (Binder): include explanation of commented-out options.
      * examples/microbit/simple_buttons_dispatcher.ads (Storage_Size):
          increased to 2560, so as to get the secondary stack size up to 256
          (see GCC PR108801).
      * examples/stm32f4/.gitignore: added Ada_Drivers_Library, alire, config.
      * examples/stm32f4/alire.toml: new; mainly copied from ../microbit, but
          RTS is set to embedded-stm32f4, available with gnat_arm_elf.
      * examples/stm32f4/Makefile: include changes made to ../microbit/Makefile.
          (RTS): set to embedded-stm32f4.
      * examples/stm32f4/simple_buttons.gpr (context): added Ada_Drivers_Library.
        (Binder): extends Options.Ravenscar.Binder, adds switches to attempt
          to control the size of the executable (still much much larger than
          cortex-gnat-rts).
      * examples/stm32f4/simple_buttons.prj: added case exceptions.
      * examples/stm32f4/Digital_IO.STM32F4.impl/digital_io-stm32f4_support.adb:
          rework to use Ada Drivers Library.

--- examples/microbit/simple_buttons_dispatcher.ads
+++ examples/microbit/simple_buttons_dispatcher.ads
@@ -6,7 +6,7 @@ package Simple_Buttons_Dispatcher is
    --  The ColdFrame event queue.
    Dispatcher : aliased ColdFrame.Events.Standard.Event_Queue_Base
      (Priority             => System.Default_Priority,
-      Storage_Size         => 2048,
+      Storage_Size         => 2560, -- PR108801
       Secondary_Stack_Size => 256,
       Capacity             => 16);
 
--- examples/stm32f4/Digital_IO.STM32F4.impl/digital_io-stm32f4_support.adb
+++ examples/stm32f4/Digital_IO.STM32F4.impl/digital_io-stm32f4_support.adb
@@ -14,90 +14,54 @@
 --  Jack Ganssle's paper "A Guide to Debouncing" at
 --  https://pubweb.eng.utah.edu/~cs5780/debouncing.pdf.
 
+pragma Warnings (Off, "referenced");
+
 with Ada.Real_Time;
 with Digital_IO.Input_Signal_State_Callback;
-with STM32F40x.GPIO;   use STM32F40x.GPIO;
-with STM32F40x.RCC;    use STM32F40x.RCC;
+with STM32.Device;
+with STM32.GPIO;
 
 package body Digital_IO.STM32F4_Support is
 
-   package GPIO_Configuration is
+   type LED is (Green, Orange, Red, Blue);
+   for LED use (Green  => 12,
+                Orange => 13,
+                Red    => 14,
+                Blue   => 15);
 
-      type EXTI_Line is mod 16;
-      --  so named in case we need to use interrupts; not practical if
-      --  we're looking for both falling and rising edges, because of
-      --  too-rapid changes.
+   LEDs : STM32.GPIO.GPIO_Points
+     := (Green'Enum_Rep  => (Periph => STM32.Device.GPIO_D'Access,
+                             Pin => STM32.GPIO.Pin_12),
+         Orange'Enum_Rep => (Periph => STM32.Device.GPIO_D'Access,
+                             Pin => STM32.GPIO.Pin_13),
+         Red'Enum_Rep    => (Periph => STM32.Device.GPIO_D'Access,
+                             Pin => STM32.GPIO.Pin_14),
+         Blue'Enum_Rep   => (Periph => STM32.Device.GPIO_D'Access,
+                             Pin => STM32.GPIO.Pin_15));
 
-      type Used_Input_Signal (Valid : Boolean := False) is record
-         case Valid is
-            when False => null;
-            when True  => Signal : Input_Signal;
-         end case;
-      end record;
-      Line_To_Input_Signal :
-        constant array (EXTI_Line) of Used_Input_Signal :=
-        (0      => (True, 0),
-         others => <>);
-
-      type GPIO_Access is access all GPIO_Peripheral;
-
-      type GPIO_Index is (A, B, C, D, E, F, G, H, I)
-      with
-        Warnings => Off;
-      --  Note, 'Pos is the bit number in AHB1ENR, and the number to
-      --  set in EXTICR to associate the Line with the GPIO.
-
-      GPIOs : constant array (GPIO_Index) of GPIO_Access :=
-        (GPIOA_Periph'Access,
-         GPIOB_Periph'Access,
-         GPIOC_Periph'Access,
-         GPIOD_Periph'Access,
-         GPIOE_Periph'Access,
-         GPIOF_Periph'Access,
-         GPIOG_Periph'Access,
-         GPIOH_Periph'Access,
-         GPIOI_Periph'Access);
-
-      type Used_Line (Valid : Boolean := False) is record
-         case Valid is
-            when False =>
-               null;
-            when True  =>
-               GPIO : GPIO_Index;
-               Line : EXTI_Line;
-         end case;
-      end record;
-
-      Input_Signal_To_Line : constant array (Input_Signal) of Used_Line :=
-        (0      => (True, A, 0),  -- user button
-         others => <>);
-
-      Output_Signal_To_Line : constant array (Output_Signal) of Used_Line :=
-        (0      => (True, D, 14),  -- red
-         1      => (True, D, 12),  -- green
-         2      => (True, D, 15),  -- blue
-         3      => (True, D, 13),  -- orange
-         others => <>);
-
-      --  How often to check for input change
-      Debounce_Interval : constant Ada.Real_Time.Time_Span
-        := Ada.Real_Time.Milliseconds (5);
-
-      --  Number of successive inputs that must be the same to
-      --  recognise a change
-      Debounce_Count : constant := 8;
-
-   end GPIO_Configuration;
-   use GPIO_Configuration;
+   --  There's only one Lamp and one Button
+   Lamp : STM32.GPIO.GPIO_Point := LEDs (Red'Enum_Rep);
+   Button : constant STM32.GPIO.GPIO_Point :=
+     (Periph => STM32.Device.GPIO_A'Access,
+      Pin    => STM32.GPIO.Pin_0);
+   Input_Signal_For_Button : constant := 0;
 
    task Heartbeat;
    --  Flash the green LED
+
+   --  How often to check for input change
+   Debounce_Interval : constant Ada.Real_Time.Time_Span
+     := Ada.Real_Time.Milliseconds (5);
+
+   --  Number of successive inputs that must be the same to
+   --  recognise a change
+   Debounce_Count : constant := 8;
 
    task Debounce_Inputs;
 
    procedure Configure_Inputs;
    procedure Configure_Outputs;
-   procedure Enable_GPIO (Which : GPIO_Index);
+
    function Get_Input_State
      (For_Input : Digital_IO_Support.Input_Signal)
      return Boolean;
@@ -114,41 +78,32 @@ package body Digital_IO.STM32F4_Support is
          Index : Debounce_Index := Debounce_Index'First;
          Debounced : Boolean := False;
       end record;
-      Inputs : array (Input_Signal_To_Line'Range) of Input_Debouncing;
+      Button_Input : Input_Debouncing; -- one button
       Next : Ada.Real_Time.Time := Ada.Real_Time.Clock;
       use type Ada.Real_Time.Time;
    begin
       loop
          Next := Next + Debounce_Interval;
          delay until Next;
-         for L in Inputs'Range loop
-            if Input_Signal_To_Line (L).Valid then
-               declare
-                  Signal : constant Input_Signal
-                    := Line_To_Input_Signal (EXTI_Line (L)).Signal;
-               begin
-                  Inputs (L).Index := Inputs (L).Index + 1;
-                  Inputs (L).Inputs (Inputs (L).Index)
-                    := Get_Input_State
-                      (Digital_IO_Support.Input_Signal (Signal));
-                  if Inputs (L).Inputs = Successive_Inputs'(others => True)
-                    and Inputs (L).Debounced = False
-                  then
-                     Inputs (L).Debounced := True;
-                     Input_Signal_State_Callback.Call_Callbacks
-                       ((S     => Signal,
-                         State => True));
-                  elsif Inputs (L).Inputs = Successive_Inputs'(others => False)
-                    and Inputs (L).Debounced = True
-                  then
-                     Inputs (L).Debounced := False;
-                     Input_Signal_State_Callback.Call_Callbacks
-                       ((S     => Signal,
-                         State => False));
-                  end if;
-               end;
-            end if;
-         end loop;
+
+         Button_Input.Index := Button_Input.Index + 1;
+         Button_Input.Inputs (Button_Input.Index)
+           := Button.Set;
+         if Button_Input.Inputs = Successive_Inputs'(others => True)
+           and Button_Input.Debounced = False
+         then
+            Button_Input.Debounced := True;
+            Input_Signal_State_Callback.Call_Callbacks
+              ((S     => Input_Signal_For_Button,
+                State => True));
+         elsif Button_Input.Inputs = Successive_Inputs'(others => False)
+           and Button_Input.Debounced = True
+         then
+            Button_Input.Debounced := False;
+            Input_Signal_State_Callback.Call_Callbacks
+              ((S     => Input_Signal_For_Button,
+                State => False));
+         end if;
       end loop;
    end Debounce_Inputs;
 
@@ -158,17 +113,17 @@ package body Digital_IO.STM32F4_Support is
 
       --  flash for 1 second at startup
       for J in 1 .. 5 loop
-         Set_Output_State (1, False);
+         STM32.GPIO.Clear (LEDs (Green'Enum_Rep));
          delay until Ada.Real_Time.Clock + Ada.Real_Time.Milliseconds (100);
-         Set_Output_State (1, True);
+         STM32.GPIO.Set (LEDs (Green'Enum_Rep));
          delay until Ada.Real_Time.Clock + Ada.Real_Time.Milliseconds (100);
       end loop;
 
       --  flash every second while running
       loop
-         Set_Output_State (1, False);
+         STM32.GPIO.Clear (LEDs (Green'Enum_Rep));
          delay until Ada.Real_Time.Clock + Ada.Real_Time.Milliseconds (900);
-         Set_Output_State (1, True);
+         STM32.GPIO.Set (LEDs (Green'Enum_Rep));
          delay until Ada.Real_Time.Clock + Ada.Real_Time.Milliseconds (100);
       end loop;
    end Heartbeat;
@@ -181,81 +136,28 @@ package body Digital_IO.STM32F4_Support is
    end Initialize;
 
    procedure Configure_Inputs is
+      use STM32.Device;
+      use STM32.GPIO;
    begin
-      for J in Input_Signal'Range loop
-         if Input_Signal_To_Line (J).Valid then
-            Enable_GPIO (Input_Signal_To_Line (J).GPIO);
-
-            declare
-               Which_GPIO : constant GPIO_Index
-                 := Input_Signal_To_Line (J).GPIO;
-               GPIO : GPIO_Peripheral renames
-                 GPIOs (Which_GPIO).all;
-               Line : constant Natural :=
-                 Natural (Input_Signal_To_Line (J).Line);
-            begin
-               --  Don't need to configure the line as an input, because
-               --  that's the default.
-
-               --  Don't need to configure OTYPER, because the pin is an
-               --  input.
-
-               --  Don't need to configure OSPEEDR, because the pin is an
-               --  input.
-
-               --  Configure PUPDR for neither pull-up nor -down (the
-               --  user pushbutton has an internal pull-down
-               --  resistor). 0 => released, 1 => pushed.
-               GPIO.PUPDR.Arr (Line) := 0;
-            end;
-         end if;
-      end loop;
+      Enable_Clock (Button.Periph.all);
+      Configure_IO (Button,
+                    Config => (Mode      => Mode_In,
+                               Resistors => Pull_Down));
    end Configure_Inputs;
 
    procedure Configure_Outputs is
+      use STM32.Device;
+      use STM32.GPIO;
    begin
-      for J in Output_Signal'Range loop
-         if Output_Signal_To_Line (J).Valid then
-            Enable_GPIO (Output_Signal_To_Line (J).GPIO);
-
-            --  Configure the line as an output.
-            declare
-               GPIO : GPIO_Peripheral renames
-                 GPIOs (Output_Signal_To_Line (J).GPIO).all;
-               Line : constant Natural :=
-                 Natural (Output_Signal_To_Line (J).Line);
-            begin
-               GPIO.MODER.Arr (Line) := 1;
-            end;
-
-            --  Don't need to configure OTYPER, because the default is
-            --  push-pull.
-
-            --  Don't need to configure OSPEEDR, because the default
-            --  is slow, which is (I expect) more than adequate for an
-            --  LED!
-
-            --  Don't need to configure PUPDR, because the default is
-            --  no pull-up, no pull-down.
-         end if;
+      for L of LEDs loop
+         Enable_Clock (L.Periph.all);  -- overkill!
       end loop;
+      Configure_IO (LEDs,
+                    Config => (Mode      => Mode_Out,
+                               Resistors => Pull_Up,
+                               others    => <>));
+      Clear (LEDs);
    end Configure_Outputs;
-
-   procedure Enable_GPIO (Which : GPIO_Index) is
-   begin
-      case Which is
-         when A => RCC_Periph.AHB1ENR.GPIOAEN := 1;
-         when B => RCC_Periph.AHB1ENR.GPIOBEN := 1;
-         when C => RCC_Periph.AHB1ENR.GPIOCEN := 1;
-         when D => RCC_Periph.AHB1ENR.GPIODEN := 1;
-         when E => RCC_Periph.AHB1ENR.GPIOEEN := 1;
-         when F => RCC_Periph.AHB1ENR.GPIOFEN := 1;
-         when G => RCC_Periph.AHB1ENR.GPIOGEN := 1;
-         when H => RCC_Periph.AHB1ENR.GPIOHEN := 1;
-         when I => RCC_Periph.AHB1ENR.GPIOIEN := 1;
-      end case;
-   end Enable_GPIO;
-
 
    function Get (This      : Implementation;
                  For_Input : Digital_IO_Support.Input_Signal)
@@ -279,45 +181,25 @@ package body Digital_IO.STM32F4_Support is
      (For_Input : Digital_IO_Support.Input_Signal)
      return Boolean
    is
+      use type Digital_IO_Support.Input_Signal;
+      pragma Assert (For_Input = 0, "only input 0 supported");
    begin
-      if Input_Signal_To_Line (Input_Signal (For_Input)).Valid then
-         declare
-            Line : constant Natural :=
-              Natural (Input_Signal_To_Line (Input_Signal (For_Input)).Line);
-            GPIO : GPIO_Peripheral renames
-              GPIOs (Input_Signal_To_Line (Input_Signal (For_Input)).GPIO).all;
-            use type STM32F40x.Bit;
-         begin
-            --  The 'pushed' state is logic 1, because we're pulled down.
-            --  XXX this should be parameterised!
-            return GPIO.IDR.IDR.Arr (Line) /= 0;
-         end;
-      else
-         return False;
-      end if;
+      return Button.Set;
    end Get_Input_State;
 
    procedure Set_Output_State
      (For_Output : Digital_IO_Support.Output_Signal;
       To         : Boolean)
    is
+      use type Digital_IO_Support.Output_Signal;
+      pragma Assert (For_Output = 0, "only output 0 supported");
    begin
-      if Output_Signal_To_Line (Output_Signal (For_Output)).Valid then
-         declare
-            Line : constant Natural :=
-              Natural (Output_Signal_To_Line
-                         (Output_Signal (For_Output)).Line);
-            GPIO : GPIO_Peripheral renames
-              GPIOs (Output_Signal_To_Line
-                       (Output_Signal (For_Output)).GPIO).all;
-         begin
-            if To then
-               GPIO.BSRR.BS.Arr (Line) := 1;
-            else
-               GPIO.BSRR.BR.Arr (Line) := 1;
-            end if;
-         end;
+      if To then
+         LEDs (Red'Enum_Rep).Set;
+      else
+         LEDs (Red'Enum_Rep).Clear;
       end if;
    end Set_Output_State;
+
 
 end Digital_IO.STM32F4_Support;
